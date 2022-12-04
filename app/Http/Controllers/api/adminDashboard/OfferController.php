c7b31b481d9a4a75f58a7635fe435e198f7c5970
R <RuqaiahSaif.com>
2022-12-04 16:52:56 +0300

    update_offer

--- app/Http/Controllers/api/adminDashboard/OfferController.php
+++ app/Http/Controllers/api/adminDashboard/OfferController.php
@@ -63,8 +63,13 @@ class OfferController extends BaseController
               'coupon_status' =>'required_if:offer_type,fixed_amount,percent',
              'discount_value_offer3' =>'required_if:offer_type,percent',
             'maximum_discount' =>'required_if:offer_type,percent',
-            'get' =>'required_if:offer_type,percent'
-
+            'product_id'=>['required_if:offer_type,If_bought_gets','required_if:category_id,null'],
+            'get_product_id' =>'',
+            'category_id'=>['required_if:offer_type,If_bought_gets','required_if:product_id,null'],
+            'get_category_id'=>"",
+            'select_product_id'=>"",
+            'select_category_id'=>"",
+            'select_payment_id'=>"",
         ]);
         if ($validator->fails())
         {
@@ -88,7 +93,7 @@ class OfferController extends BaseController
              'offer_amount_minimum' => $request->offer_amount_minimum,
              'coupon_status' => $request->coupon_status,
              'discount_value_offer3' => $request->discount_value_offer3,
-             'maximum_discount' => $request->maximum_discount
+             'maximum_discount' => $request->maximum_discount,
           ]);
           if($request->offer_type=="If_bought_gets"){
           switch($request->purchase_type) {
@@ -113,7 +118,7 @@ class OfferController extends BaseController
             $offer->categories()->attach(explode(',', $request->select_category_id),["type" => "select"]);
             break;
              case('selected_payment'):
-            $offer->paymenttypes()->attach(explode(',', $request->select_payment_id));
+            $offer->paymenttypes()->attach(explode(',', $request->select_payment_id),["type" => "select"]);
             break;
 
           }
@@ -123,6 +128,7 @@ class OfferController extends BaseController
 
          return $this->sendResponse($success,'تم إضافة العرض بنجاح','offer Added successfully');
     }
+
 
     /**
      * Display the specified resource.
@@ -185,7 +191,93 @@ class OfferController extends BaseController
      */
     public function update(Request $request, Offer $offer)
     {
-        //
+        $input = $request->all();
+        $validator =  Validator::make($input ,[
+            'offer_type'=>'required',
+            'offer_title'=>'required|string',
+            'offer_view' =>'required|string',
+            'start_at'=>'required|date',
+            'end_at'=>'required|date',
+            'purchase_quantity' =>"required_if:offer_type,If_bought_gets",
+            'purchase_type' =>'required_if:offer_type,If_bought_gets',
+            'get_quantity' =>'required_if:offer_type,If_bought_gets|numeric',
+            'get_type' =>'required_if:offer_type,If_bought_gets',
+            'offer1_type' =>'required_if:offer_type,If_bought_gets',
+            'discount_percent' =>'required_if:offer1_type,percent',
+            'discount_value_offer2' =>'required_if:offer_type,fixed_amount',
+            'offer_apply' =>'required_if:offer_type,fixed_amount,percent',
+            'offer_type_minimum' =>'required_if:offer_type,fixed_amount,percent',
+             'offer_amount_minimum' =>'required_if:offer_type,fixed_amount,percent',
+              'coupon_status' =>'required_if:offer_type,fixed_amount,percent',
+             'discount_value_offer3' =>'required_if:offer_type,percent',
+            'maximum_discount' =>'required_if:offer_type,percent',
+            'product_id'=>"",
+            'get_product_id' =>"",
+            'category_id'=>"",
+            'get_category_id'=>"",
+            'select_product_id'=>"",
+            'select_category_id'=>"",
+            'select_payment_id'=>"",
+        ]);
+        if ($validator->fails())
+        {
+            return $this->sendError(null,$validator->errors());
+        }
+        $offer->update([
+            'offer_type' => $request->input('offer_type'),
+            'offer_title' => $request->input('offer_title'),
+            'offer_view' => $request->input('offer_view'),
+            'start_at' => $request->input('start_at'),
+            'end_at' => $request->input('end_at'),
+            'purchase_quantity' => $request->input('purchase_quantity'),
+            'purchase_type' => $request->input('purchase_type'),
+            'get_quantity' => $request->input('get_quantity'),
+            'get_type' => $request->input('get_type'),
+            'offer1_type' => $request->input('offer1_type'),
+            'discount_percent' => $request->input('discount_percent'),
+            'discount_value_offer2' => $request->input('discount_value_offer2'),
+            'offer_apply' => $request->input('offer_apply'),
+            'offer_type_minimum' => $request->input('offer_type_minimum'),
+            'offer_amount_minimum' => $request->input('offer_amount_minimum'),
+            'coupon_status' => $request->input('coupon_status'),
+            'discount_value_offer3' => $request->input('discount_value_offer3'),
+            'maximum_discount' => $request->input('maximum_discount'),
+             
+          ]);
+      
+          if($request->offer_type=="If_bought_gets"){
+         
+          switch($request->purchase_type) {
+            case('category'):
+                $offer->categories()->sync(explode(',', $request->category_id),["type" => "buy"]);
+                $offer->categories()->sync(explode(',', $request->get_category_id),["type" =>"get" ]);
+
+                break;
+            case('product'):
+               $offer->products()->sync(explode(',', $request->product_id),["type" => "buy"]);
+                $offer->products()->sync(explode(',', $request->get_product_id),["type" => "get"]);
+            break;
+
+            }
+        }
+        else{
+            switch($request->offer_apply) {
+            case('selected_product'):
+                $offer->products()->sync(explode(',', $request->select_product_id),["type" => "select"]);
+                break;
+            case('selected_category'):
+            $offer->categories()->sync(explode(',', $request->select_category_id),["type" => "select"]);
+            break;
+             case('selected_payment'):
+            $offer->paymenttypes()->sync(explode(',', $request->select_payment_id),["type" => "select"]);
+            break;
+
+          }
+        }
+         $success['offers']=New OfferResource($offer);
+        $success['status']= 200;
+
+         return $this->sendResponse($success,'تم التعديل العرض بنجاح','offer updated successfully');
     }
 
     /**
