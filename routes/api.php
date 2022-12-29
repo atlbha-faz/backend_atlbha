4a0c03cb4ebeff8bd9884653b1d5d35467be43ba
atlbha <117630530+atlbha@users.noreply.github.com>
2022-12-29 09:21:58 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -133,7 +133,7 @@ Route::resource('offer',App\Http\Controllers\api\adminDashboard\OfferController:
 Route::resource('product',App\Http\Controllers\api\adminDashboard\ProductController::class);
 Route::resource('option',App\Http\Controllers\api\adminDashboard\OptionController::class);
 Route::resource('user',App\Http\Controllers\api\adminDashboard\UserController::class);
-Route::post('storeReport', [App\Http\Controllers\api\adminDashboard\StoreReportController::class,'index']);
+Route::get('storeReport', [App\Http\Controllers\api\adminDashboard\StoreReportController::class,'index']);
 Route::get('registration_status_show', [App\Http\Controllers\api\adminDashboard\SettingController::class,'registration_status_show']);
 Route::post('registration_status_update', [App\Http\Controllers\api\adminDashboard\SettingController::class,'registration_status_update']);
 
