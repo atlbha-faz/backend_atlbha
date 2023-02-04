41cf7c52de36217bf14700674c879f351ac4e0a6
atlbha <117630530+atlbha@users.noreply.github.com>
2023-02-04 11:45:32 +0200

    Update PackageResource.php

--- app/Http/Resources/PackageResource.php
+++ app/Http/Resources/PackageResource.php
@@ -25,7 +25,7 @@ class PackageResource extends JsonResource
             'is_deleted' => $this->is_deleted!==null ? $this->is_deleted:0,
             'plans'=>PlanResource::collection($this->plans),
             'templates'=>TemplateResource::collection($this->templates),
-            'stores'=> StoreResource::collection($this->stores)
+           // 'stores'=> StoreResource::collection($this->stores)
 
         ];
     }
