00b77f0abacf62c60b1379c2dbad24a4ff3e8f02
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2023-02-08 21:18:41 +0300

    verification

---
+++ app/Http/Controllers/api/adminDashboard/SubscriptionsController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Http\Controllers\api\adminDashboard;
+
+use App\Models\Note;
+use App\Models\User;
+use App\Models\Alert;
+use App\Models\Store;
+use App\Models\Package;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Models\Package_store;
+use App\Events\VerificationEvent;
+use Illuminate\Support\Facades\DB;
+use App\Http\Resources\NoteResource;
+use App\Http\Resources\AlertResource;
+use App\Http\Resources\StoreResource;
+use App\Notifications\emailNotification;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Notification;
+use App\Http\Resources\SubscriptionsResource;
+use App\Notifications\verificationNotification;
+use App\Http\Controllers\api\BaseController as BaseController;
+
+class SubscriptionsController extends BaseController
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+    public function index()
+    {
+
+        $success['stores']=SubscriptionsResource::collection(Store::where('is_deleted',0,)->where('package_id','!=',null)->get());
+        $success['status']= 200;
+
+         return $this->sendResponse($success,'تم ارجاع المتاجر بنجاح','Subscriptions return successfully');
+    }
+    public function deleteall(Request $request)
+    {
+
+            $stores =Store::whereIn('id',$request->id)->get();
+        
+           foreach($stores as $store)
+           {
+             if (is_null($store) || $store->is_deleted==1 ){
+                    return $this->sendError("الاشتراك غير موجودة","Subscriptions is't exists");
+             }
+             $store_package=Package_store::where('package_id',$store->package_id)->first();
+             if (is_null($store_package) || $store_package->is_deleted==1){
+            }
+            else{
+             $store_package->update(['is_deleted' => 1]);
+            }
+             $store->update(['package_id' => null]);
+            $success['Subscriptions']=New SubscriptionsResource($store);
+
+            }
+
+           $success['status']= 200;
+
+            return $this->sendResponse($success,'تم حذف الاشتراك بنجاح','Subscriptions deleted successfully');
+    }
+       public function changeSatusall(Request $request)
+            {
+
+                    $stores =Store::whereIn('id',$request->id)->get();
+                foreach($stores as $store)
+                {
+                    if (is_null($store) || $store->is_deleted==1){
+                        return $this->sendError("  الاشتراك غير موجودة","Subscriptions is't exists");
+              }
+              $store_package=Package_store::where('package_id',$store->package_id)->first();
+              if( $store->package_id != null){
+              $store->update(['package_id' => null]);
+              }
+              if (is_null($store_package) || $store_package->is_deleted==1){
+              }
+              else{
+                    if($store_package->status === 'active'){
+                        $store_package->update(['status' => 'not_active']);
+                }
+                else{
+                    $store_package->update(['status' => 'active']);
+                }
+            }
+                $success['Subscriptions']= New SubscriptionsResource($store);
+
+                    }
+                    $success['status']= 200;
+
+                return $this->sendResponse($success,'تم تعديل حالة الاشتراك بنجاح','Subscriptions updated successfully');
+           }
+
+           public function addAlert(Request $request)
+           {
+               $input = $request->all();
+               $validator =  Validator::make($input ,[
+                   'type'=>'required|in:now,after',
+                   'subject'=>'required|string|max:255',
+                   'message'=>'required|string',
+                   'store_id'=>'exists:stores,id',
+       
+               ]);
+               if ($validator->fails())
+               {
+                   return $this->sendError(null,$validator->errors());
+               }
+               
+               $data = [
+                   'subject' => $request->subject,
+                   'message' => $request->message,
+                   'store_id' => $request->store_id,
+             
+               ];
+               if($request->type =="now"){
+               $alert= Alert::create( [ 
+               'subject' => $request->subject,
+               'message' => $request->message,
+               'store_id' => $request->store_id]);
+               }
+               else{
+                   $alert= Alert::create( [ 
+                   'subject' => $request->subject,
+                   'message' => $request->message,
+                   'store_id' => $request->store_id,
+                   'start_at' =>$request->start_at,
+                   'end_at' =>$request->end_at,
+               ]);
+       
+               }
+               $users = User::where('store_id',$request->store_id)->where('user_type','store')->get();
+              foreach($users as  $user)
+              {
+                 Notification::send($user , new emailNotification($data));
+              }
+                $success['alerts']=New AlertResource($alert);
+               $success['status']= 200;
+       
+                return $this->sendResponse($success,'تم إضافة بنجاح',' Added successfully');
+           }
+       
+}
---
+++ app/Http/Resources/VerificationResource.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class VerificationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+       'id' =>$this->id,
+       'store_name'=>$this->store_name,
+       'domain'=>$this->domain,
+       'phonenumber'=>$this->phonenumber,
+       'store_email'=>$this->store_email,
+       'icon' =>$this->icon,
+       'description'=>$this->description,
+       'business_license'=>$this->business_license,
+       'ID_file' =>$this->ID_file,
+       'snapchat'=>$this->snapchat,
+       'facebook' =>$this->facebook,
+       'twiter'=>$this->twiter,
+       'youtube'=>$this->youtube,
+       'instegram' =>$this->instegram,
+       'logo'=>$this->logo,
+       'entity_type'=>$this->entity_type,
+       'user' =>New UserResource($this->user),
+       'activity' =>ActivityResource::collection($this->activities),
+       'country' => New CountryResource($this->country),
+       'city' => New CityResource($this->city),
+       'rate'=> $this->rate($this->id)!==null ? $this->rate($this->id):0,
+       'periodtype'=>$this->periodtype,
+       'left'=>$this->left($this->id),
+       'verification_status'=>$this->verification_status !==null ? $this->verification_status:'pending',
+       'status' => $this->status !==null ? $this->status:'active',
+       'special' => $this->special !==null ? $this->special:'not_special' ,
+       'is_deleted' => $this->is_deleted!==null ? $this->is_deleted:0,
+   ];
+   }
+}
