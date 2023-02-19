a58eaade78147fb0da389d8cae5b6d3830cb57d9
atlbha <117630530+atlbha@users.noreply.github.com>
2023-02-19 11:46:00 +0200

    Update SectionResource.php

--- app/Http/Resources/SectionResource.php
+++ app/Http/Resources/SectionResource.php
@@ -14,10 +14,16 @@ class SectionResource extends JsonResource
      */
     public function toArray($request)
     {
+        if($this->status ==null || $this->status == 'active'){
+            $status = 'نشط';
+        }else{
+            $status = 'غير نشط';
+        }
+        
         return [
             'id' =>$this->id,
             'name' =>$this->name,
-            'status' => $this->status !==null ? $this->status:'active',
+            'status' => $status,
             'is_deleted' => $this->is_deleted!==null ? $this->is_deleted:0,
             'is_deleted' => $this->is_deleted,
             'created_at' => (string) $this->created_at,
