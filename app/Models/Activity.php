3da49f4c41f25bcbe915b3400b351ee5c1db7e54
atlbha <117630530+atlbha@users.noreply.github.com>
2023-02-19 11:10:38 +0200

    Update Activity.php

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Activity extends Model
 {
     use HasFactory;
-         protected $fillable = ['name','status','is_deleted'];
+         protected $fillable = ['name','icon','status','is_deleted'];
 
 
  public function stores()
@@ -20,5 +20,26 @@ class Activity extends Model
         'store_id'
         );
   }
+    
+    public function setIconAttribute($icon)
+    {
+        if (!is_null($icon)) {
+            if (gettype($icon) != 'string') {
+                $i = $icon->store('images/icon/activity', 'public');
+                $this->attributes['icon'] = $icon->hashName();
+            } else {
+                $this->attributes['icon'] = $icon;
+            }
+        }
+    }
+
+    public function getIconAttribute($icon)
+    {
+        if (is_null($icon)) {
+            return   asset('assets/media/man.png');
+        }
+        return asset('storage/images/icon/activity') . '/' . $icon;
+    }
+    
 }
 
