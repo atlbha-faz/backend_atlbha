e48b0226b901b90d72cc57d5363803690cbf1bec
atlbha <117630530+atlbha@users.noreply.github.com>
2023-02-19 11:31:10 +0200

    Update CommentResource.php

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -14,11 +14,17 @@ class CommentResource extends JsonResource
      */
     public function toArray($request)
     {
+        if($this->status ==null || $this->status == 'active'){
+            $status = 'نشط';
+        }else{
+            $status = 'غير نشط';
+        }
+        
         return [
             'id' =>$this->id,
             'comment_text' => $this->comment_text,
             'rateing' => $this->rateing,
-            'status' => $this->status !==null ? $this->status:'active',
+            'status' => $status,
             'is_deleted' => $this->is_deleted!==null ? $this->is_deleted:0,
             'created_at' => (string) $this->created_at->diffForHumans(),
             'updated_at' => (string) $this->updated_at,
