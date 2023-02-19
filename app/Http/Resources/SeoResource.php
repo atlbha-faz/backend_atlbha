61982c729d2d30afb585c805f3ac661504cd72f1
atlbha <117630530+atlbha@users.noreply.github.com>
2023-02-19 11:46:20 +0200

    Update SeoResource.php

--- app/Http/Resources/SeoResource.php
+++ app/Http/Resources/SeoResource.php
@@ -14,6 +14,12 @@ class SeoResource extends JsonResource
      */
     public function toArray($request)
     {
+        if($this->status ==null || $this->status == 'active'){
+            $status = 'نشط';
+        }else{
+            $status = 'غير نشط';
+        }
+        
         return [
             'id' =>$this->id,
             'index_page_title' => $this->index_page_title,
@@ -22,7 +28,7 @@ class SeoResource extends JsonResource
             'link' => $this->link,
            'robots' => $this->robots,
             'key_words' => explode(',',$this->key_words),
-            'status' => $this->status !==null ? $this->status:'active',
+            'status' => $status,
             'is_deleted' => $this->is_deleted!==null ? $this->is_deleted:0,
             'created_at' => (string) $this->created_at,
             'updated_at' => (string) $this->updated_at,
