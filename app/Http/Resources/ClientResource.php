b956277ba0f2eb2df150108f401da645f3f3ef48
atlbha <117630530+atlbha@users.noreply.github.com>
2023-02-19 11:30:49 +0200

    Update ClientResource.php

--- app/Http/Resources/ClientResource.php
+++ app/Http/Resources/ClientResource.php
@@ -14,6 +14,13 @@ class ClientResource extends JsonResource
      */
     public function toArray($request)
     {
+        
+        if($this->status ==null || $this->status == 'active'){
+            $status = 'نشط';
+        }else{
+            $status = 'غير نشط';
+        }
+        
         return [
             'id' =>$this->id,
             'ID_number' => $this->ID_number,
@@ -23,7 +30,7 @@ class ClientResource extends JsonResource
             'image' => $this->image,
             'gender' => $this->gender,
             'phonenumber' => $this->phonenumber,
-            'status' => $this->status !==null ? $this->status:'active',
+            'status' => $status,
             'is_deleted' => $this->is_deleted!==null ? $this->is_deleted:0,
             'created_at' => (string) $this->created_at,
             'updated_at' => (string) $this->updated_at,
