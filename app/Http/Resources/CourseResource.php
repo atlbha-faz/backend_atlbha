734dff7158d58b9fe9a3e2ebf7c122561a63417f
atlbha <117630530+atlbha@users.noreply.github.com>
2023-02-19 11:32:31 +0200

    Update CourseResource.php

--- app/Http/Resources/CourseResource.php
+++ app/Http/Resources/CourseResource.php
@@ -16,13 +16,18 @@ class CourseResource extends JsonResource
      */
     public function toArray($request)
     {
+        if($this->status ==null || $this->status == 'active'){
+            $status = 'نشط';
+        }else{
+            $status = 'غير نشط';
+        }
         return [
             'id' =>$this->id,
             'name' => $this->name,
             'description' => $this->description,
             'tags' => explode(',',$this->tags),
             'duration' => $this->duration,
-            'status' => $this->status !==null ? $this->status:'active',
+            'status' => $status,
             'is_deleted' => $this->is_deleted!==null ? $this->is_deleted:0,
             'created_at' => (string) $this->created_at,
             'updated_at' => (string) $this->updated_at,
