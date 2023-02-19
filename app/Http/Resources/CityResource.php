c00af22908e5cbc32910f54363d07689c7f73516
atlbha <117630530+atlbha@users.noreply.github.com>
2023-02-19 11:30:21 +0200

    Update CityResource.php

--- app/Http/Resources/CityResource.php
+++ app/Http/Resources/CityResource.php
@@ -15,12 +15,17 @@ class CityResource extends JsonResource
      */
     public function toArray($request)
     {
+        if($this->status ==null || $this->status == 'active'){
+            $status = 'نشط';
+        }else{
+            $status = 'غير نشط';
+        }
         return [
             'id' =>$this->id,
             'name' => $this->name,
             'name_en' => $this->name_en,
             'code' => $this->code,
-            'status' => $this->status !==null ? $this->status:'active',
+            'status' => $status,
             'is_deleted' => $this->is_deleted!==null ? $this->is_deleted:0,
             'created_at' => (string) $this->created_at,
             'updated_at' => (string) $this->updated_at,
