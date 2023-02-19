bfb1515806f0e80487c1fb554a154511c0cfa762
atlbha <117630530+atlbha@users.noreply.github.com>
2023-02-19 11:43:58 +0200

    Update PaymenttypeResource.php

--- app/Http/Resources/PaymenttypeResource.php
+++ app/Http/Resources/PaymenttypeResource.php
@@ -14,11 +14,16 @@ class PaymenttypeResource extends JsonResource
      */
     public function toArray($request)
     {
+        if($this->status ==null || $this->status == 'active'){
+            $status = 'نشط';
+        }else{
+            $status = 'غير نشط';
+        }
         return [
         'id' =>$this->id,
         'name'=>$this->name,
          'image'=>$this->image,
-         'status' => $this->status !==null ? $this->status:'active',
+         'status' => $status,
          'is_deleted' => $this->is_deleted!==null ? $this->is_deleted:0,
     ];
     }
