b1715b58e404f5830b53e76f0b0133c7206424bb
atlbha <117630530+atlbha@users.noreply.github.com>
2023-02-19 11:55:27 +0200

    Update StoreResource.php

--- app/Http/Resources/StoreResource.php
+++ app/Http/Resources/StoreResource.php
@@ -15,6 +15,36 @@ class StoreResource extends JsonResource
      */
     public function toArray($request)
     {
+        if($this->status ==null || $this->status == 'active'){
+            $status = 'نشط';
+        }else{
+            $status = 'غير نشط';
+        }
+        
+        if($this->special ==null || $this->special == 'special'){
+            $special = 'مميز';
+        }else{
+            $special = 'غير مميز';
+        }
+        
+          if($this->confirmation_status ==null || $this->confirmation_status == 'request'){
+            $confirmation_status = 'لم يتم الطلب';
+        }elseif($this->confirmation_status == 'pending'){
+            $confirmation_status = 'جاري التوثيق';
+        }elseif($this->confirmation_status == 'accept'){
+            $confirmation_status = 'تم التوثيق';
+        }elseif($this->confirmation_status == 'reject'){
+            $confirmation_status = 'التوثيق مرفوض';
+        }
+        
+        
+         if($this->verification_status ==null || $this->verification_status == 'pending'){
+            $verification_status = 'قيد المعالجة';
+        }elseif($this->verification_status == 'accept'){
+            $verification_status = 'منتهي';
+        }elseif($this->verification_status == 'reject'){
+            $verification_status = 'مرفوض';
+        }
          return [
         'id' =>$this->id,
         'store_name'=>$this->store_name,
@@ -40,10 +70,10 @@ class StoreResource extends JsonResource
         'periodtype'=>$this->periodtype,
         'left'=>$this->left($this->id),
         'rate'=> $this->rate($this->id)!==null ? $this->rate($this->id):0,
-        'verification_status'=>$this->verification_status !==null ? $this->verification_status:'pending',
-        'confirmation_status'=>$this->confirmation_status !==null ? $this->confirmation_status:'request',
-        'status' => $this->status !==null ? $this->status:'active',
-        'special' => $this->special !==null ? $this->special:'not_special' ,
+        'verification_status'=>$verification_status,
+        'confirmation_status'=>$confirmation_status,
+        'status' => $status,
+        'special' => $special,
         'is_deleted' => $this->is_deleted!==null ? $this->is_deleted:0,
     ];
     }
