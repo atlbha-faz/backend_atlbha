1759f23502c9ee443b9bc31b10058673261af28b
atlbha <117630530+atlbha@users.noreply.github.com>
2023-03-02 09:30:03 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -164,7 +164,7 @@ Route::resource('paymenttype',App\Http\Controllers\api\adminDashboard\Paymenttyp
 Route::resource('comment',App\Http\Controllers\api\adminDashboard\CommentController::class);
 Route::resource('page',App\Http\Controllers\api\adminDashboard\PageController::class);
 //Route::get('relatedPage/{id}',[App\Http\Controllers\api\adminDashboard\PageController::class,"relatedPage"]);
-Route::post('page/publish',[App\Http\Controllers\api\adminDashboard\PageController::class,'publish']);
+Route::post('page-publish',[App\Http\Controllers\api\adminDashboard\PageController::class,'publish']);
 Route::resource('pagecategory',App\Http\Controllers\api\adminDashboard\PageCategoryController::class);
 Route::resource('technicalSupport',App\Http\Controllers\api\adminDashboard\TechnicalSupportController::class);
 Route::resource('currency',App\Http\Controllers\api\adminDashboard\CurrencyController::class);
@@ -287,7 +287,7 @@ Route::resource('city',App\Http\Controllers\api\storeDashboard\CityController::c
 
 // page
 Route::resource('page',App\Http\Controllers\api\storeDashboard\PageController::class);
-Route::post('pagepublish',[App\Http\Controllers\api\storeDashboard\PageController::class,'publish']);
+Route::post('page-publish',[App\Http\Controllers\api\storeDashboard\PageController::class,'publish']);
 Route::get('pagechangeSatusall',[App\Http\Controllers\api\storeDashboard\PageController::class,'changeSatusall']);
 Route::get('pagedeleteall',[App\Http\Controllers\api\storeDashboard\PageController::class,'deleteall']);
 Route::post('changePageStatus/{id}', [App\Http\Controllers\api\storeDashboard\PageController::class,'changeStatus']);
