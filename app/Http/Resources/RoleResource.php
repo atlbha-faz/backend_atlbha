0445450ff0c3957b8d5294851989d8d718973be8
atlbha <117630530+atlbha@users.noreply.github.com>
2023-03-05 07:54:36 +0200

    Update RoleResource.php

--- app/Http/Resources/RoleResource.php
+++ app/Http/Resources/RoleResource.php
@@ -14,17 +14,17 @@ class RoleResource extends JsonResource
      */
     public function toArray($request)
     {
-        if($this->status ==null || $this->status == 'active'){
+      /*  if($this->status ==null || $this->status == 'active'){
             $status = 'نشط';
         }else{
             $status = 'غير نشط';
         }
-        
+        */
         return [
         'id' =>$this->id,
         'name' => $this->name,
-        'status' => $status,
-        'is_deleted' => $this->is_deleted!==null ? $this->is_deleted:0,
+        /*'status' => $status,
+        'is_deleted' => $this->is_deleted!==null ? $this->is_deleted:0, */
         'created_at' => (string) $this->created_at,
         'updated_at' => (string) $this->updated_at
         ];
