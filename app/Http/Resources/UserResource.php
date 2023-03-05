df41fb812d538b07d5c6ee32c7e61f8614bc8b7f
atlbha <117630530+atlbha@users.noreply.github.com>
2023-03-05 08:10:49 +0200

    Update UserResource.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -36,7 +36,7 @@ class UserResource extends JsonResource
         'updated_at' => (string) $this->updated_at,
         'country' => New CountryResource($this->country),
         'city' => New CityResource($this->city),
-         'role' => (\App\Models\User::find($this->id))->role(),
+         'role' => (\App\Models\User::find($this->id))->role,
         ];
     }
 }
