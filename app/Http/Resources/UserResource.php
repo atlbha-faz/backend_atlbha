ca1c3b7c58887800f17fde0bc88d8d1f71749b0e
atlbha <117630530+atlbha@users.noreply.github.com>
2023-03-06 08:57:43 +0200

    Update UserResource.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -36,7 +36,7 @@ class UserResource extends JsonResource
         'updated_at' => (string) $this->updated_at,
         'country' => New CountryResource($this->country),
         'city' => New CityResource($this->city),
-         'role' => $this->roles->first()->name,
+         'role' => $this->roles->first(),
         ];
     }
 }
