1c0679580df549366af1e1b6b270f54ebc4ca0e1
atlbha <117630530+atlbha@users.noreply.github.com>
2023-03-06 11:41:56 +0200

    Update WebsiteorderController.php

--- app/Http/Controllers/api/adminDashboard/WebsiteorderController.php
+++ app/Http/Controllers/api/adminDashboard/WebsiteorderController.php
@@ -44,10 +44,12 @@ class WebsiteorderController extends BaseController
 })->count();
         
         
-         $array_store = array(); 
-        for($i = date("Y-m"); $i >= date("Y-m", strtotime("-6 month", date("Y-m"))); date("Y-m", strtotime("-1 month", $i))){ 
+         $array_store = array();
+        $i = date("Y-m");
+        while($i >= date("Y-m", strtotime("-6 month", date("Y-m")))){ 
             $array_store[$i]["store"]= Websiteorder::where('is_deleted',0)->where('type','store')->whereYear('created_at', date('Y', strtotime($i)))->whereMonth('created_at', date('Y', strtotime($i)))->count();
-       }
+            $i = date("Y-m", strtotime("-1 month", $i));
+        }
         $success['array_store']= $array_store;
         
         
