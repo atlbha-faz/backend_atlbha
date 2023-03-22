8c7c3a57157822f5a248c17f53d6f4e9fe5d2848
atlbha <117630530+atlbha@users.noreply.github.com>
2023-03-22 13:40:10 +0200

    Update PermissionResource.php

--- app/Http/Resources/PermissionResource.php
+++ app/Http/Resources/PermissionResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Spatie\Permission\Models\Permission;
 
 class PermissionResource extends JsonResource
 {
@@ -14,12 +15,23 @@ class PermissionResource extends JsonResource
      */
     public function toArray($request)
     {
-        
+        if($this->parent_id != null){
+            
         return [
             'id' =>$this->id,
             'name' => $this->name,
             'action_type' => $this->action_type,
             'name_ar' => $this->name_ar,
         ];
+            
+        }else{
+            return [
+            'id' =>$this->id,
+            'name' => $this->name,
+            'action_type' => $this->action_type,
+            'name_ar' => $this->name_ar,
+               'subpermissions' => PermissionResource::collection(Permission::where('parent_id',$this->id)->where('type','store')->get()),
+        ];
+        }
     }
 }
