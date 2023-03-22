f93605f11776bac769f6af7a07f5848e413b2761
atlbha <117630530+atlbha@users.noreply.github.com>
2023-03-22 14:43:05 +0200

    Update RoleController.php

--- app/Http/Controllers/api/storeDashboard/RoleController.php
+++ app/Http/Controllers/api/storeDashboard/RoleController.php
@@ -52,6 +52,57 @@ class RoleController extends BaseController
 
      return $this->sendResponse($success,'تم إضافة الأدوار بنجاح','Role Added successfully');
   }
+    
+    
+     public function update(Request $request, Role $role)
+    {
+        if (is_null($role) ||  $category->type!= 'store'){
+            return $this->sendError("الدور غير موجود"," Role is't exists");
+       }
+            $input = $request->all();
+           $validator =  Validator::make($input ,[
+                 'role_name' => 'required|string|max:255|unique:roles,name,'$role->id,
+        'permissions' => 'required|array',
+        'permissions.*' => 'nullable|exists:permissions,id',
+
+           ]);
+           if ($validator->fails())
+           {
+               # code...
+               return $this->sendError(null,$validator->errors());
+           }
+        
+         
+         
+      
+    $role = Role::create(['name'=>$request->role_name , 'type'=>'store']);
+      
+    $role->syncPermissions($request->permissions);
+         
+    $success['role']=New RoleResource($role);
+    $success['status']= 200;
+            return $this->sendResponse($success,'تم التعديل بنجاح','Role updated successfully');
+    }
+
+    
+       public function delete( Role $role)
+    {
+        if (is_null($role) ||  $category->type!= 'store'){
+            return $this->sendError("الدور غير موجود"," Role is't exists");
+       }
+           
+    $item = Role::withCount('users')->findOrFail($role->id);
+    if ($item->users_count) {
+      return $this->sendError('Permission granted to the employee', 'الصلاحية ممنوحة للموظف');
+    }
+      
+    $role->revokePermissionTo($role->permissions);
+    $role->delete();
+           
+    $success['role']=New RoleResource($role);
+    $success['status']= 200;
+            return $this->sendResponse($success,'تم الحذف بنجاح','Role updated successfully');
+    }
 
 
    
