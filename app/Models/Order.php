159c138343f70aef7f8edcaf94d395834f1b90be
atlbha <117630530+atlbha@users.noreply.github.com>
2023-03-25 13:09:11 +0300

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,11 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
-     protected $fillable = ['order_number','user_id','quantity','total_price','tax','discount','order_status','payment_status','status','is_deleted'];
+     protected $fillable = ['order_number','user_id','city_id','quantity','total_price','tax','discount','order_status','payment_status'];
 
  public function user()
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+    
+     public function city()
+    {
+        return $this->belongsTo(City::class, 'city_id', 'id');
     }
 
        public function products()
