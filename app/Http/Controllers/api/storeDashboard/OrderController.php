0f538f9aa3403cd1483b42ccb67dc9ec6232adbf
atlbha <117630530+atlbha@users.noreply.github.com>
2023-03-25 13:48:31 +0300

    Create OrderController.php

---
+++ app/Http/Controllers/api/storeDashboard/OrderController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\api\storeDashboard;
+
+use in;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use App\Http\Resources\OrderResource;
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\api\BaseController as BaseController;
+
+class OrderController extends BaseController
+{
+      public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+      {
+        $success['orders']=OrderResource::collection(Order::where('is_deleted',0)->whereHas('posts', function($q){
+    $q->where('store_id',auth()->user()->store_id);
+})->get());
+        $success['status']= 200;
+
+         return $this->sendResponse($success,'تم ارجاع الطلبات بنجاح','Orders return successfully');
+    }
+
+
+    
+}
