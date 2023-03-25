07c9196d200f09237971a39d2a02b2b48b58bb4d
atlbha <117630530+atlbha@users.noreply.github.com>
2023-03-25 13:11:32 +0300

    Update OrderItem.php

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -8,7 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     use HasFactory;
-     protected $fillable = ['order_id','product_id','quantity','price'];
+     protected $fillable = ['order_id','product_id','store_id','user_id','quantity','price','total_price','discount','order_status','payment_status'];
 
+ public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+    
+     public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+
+     public function store()
+    {
+        return $this->belongsTo(Store::class, 'store_id', 'id');
+    }
+    
+     public function city()
+    {
+        return $this->belongsTo(City::class, 'city_id', 'id');
+    }
 
 }
