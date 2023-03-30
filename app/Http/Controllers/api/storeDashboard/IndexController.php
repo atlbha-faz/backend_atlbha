b5140b065fe8e0028f1c872b4f5fe7277367aaf7
atlbha <117630530+atlbha@users.noreply.github.com>
2023-03-30 12:52:38 +0300

    Update IndexController.php

--- app/Http/Controllers/api/storeDashboard/IndexController.php
+++ app/Http/Controllers/api/storeDashboard/IndexController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\api\storeDashboard;
 
 use in;
 use App\Models\Order;
+use App\Models\User;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\OrderResource;
 use Illuminate\Contracts\Validation\Rule;
@@ -26,12 +28,12 @@ class IndexController extends BaseController
          $success['visits']=10;
         $success['customers']=User::where('user_type', 'customer')->where('status','active')->where('is_deleted',0)->where('verified',1)->count();
         
-       //  $success['sales']=DB::table('order_items')->where('order_status','completed')->where('store_id',auth()->user()->store_id)->select(DB::raw('SUM(total_price - discount) as total'));
-       //   $success['products']=Product::where('store_id',auth()->user()->store_id)->where('status','active')->where('is_deleted',0)->count();
+        $success['sales']=DB::table('order_items')->where('order_status','completed')->where('store_id',auth()->user()->store_id)->select(DB::raw('SUM(total_price - discount) as total'));
+          $success['products']=Product::where('store_id',auth()->user()->store_id)->where('status','active')->where('is_deleted',0)->count();
         
-       /* $success['orders']=OrderResource::collection(Order::whereHas('items', function($q){
+        $success['orders']=OrderResource::collection(Order::whereHas('items', function($q){
     $q->where('store_id',auth()->user()->store_id);
-})->orderBy('desc','created_at')->take(5));*/
+})->orderBy('desc','created_at')->take(5));
         
         
         $success['status']= 200;
