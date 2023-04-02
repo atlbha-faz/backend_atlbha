f33b757efa2e944b038f7af4d618b6db8758ef4c
atlbha <117630530+atlbha@users.noreply.github.com>
2023-04-02 13:17:10 +0300

    Update CartResource.php

--- app/Http/Resources/CartResource.php
+++ app/Http/Resources/CartResource.php
@@ -15,7 +15,7 @@ class CartResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'content' =>$this->content
+            'content' =>$this->content->sum('qty')
         ];
     }
 }
