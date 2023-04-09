9cf328590d79585443b7c96339599991d55742c9
atlbha <117630530+atlbha@users.noreply.github.com>
2023-04-09 14:35:46 +0300

    Update EtlobhaController.php

--- app/Http/Controllers/api/adminDashboard/EtlobhaController.php
+++ app/Http/Controllers/api/adminDashboard/EtlobhaController.php
@@ -37,7 +37,6 @@ class EtlobhaController extends BaseController
         $input = $request->all();
         $validator =  Validator::make($input ,[
             'name'=>'required|string|max:255',
-            'sku'=>'required|string|unique:products',
             'description'=>'required|string',
             'purchasing_price'=>['required','numeric','gt:0'],
             'selling_price'=>['required','numeric','gt:0'],
@@ -48,7 +47,7 @@ class EtlobhaController extends BaseController
             'images'=>'required|array',
             'images.*'=>['required','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
             'cover'=>['required','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
-            'data'=>'required|array',
+            'data'=>'nullable|array',
             'data.*.type'=>'required|in:brand,color,wight,size',
             'data.*.title'=>'required|string',
             'data.*.value'=>'required|array',
@@ -69,7 +68,6 @@ class EtlobhaController extends BaseController
         }
         $product = Product::create([
             'name' => $request->name,
-            'sku' => $request->sku,
             'for' => 'etlobha',
             'quantity' => $request->quantity,
             'less_qty' => $request->less_qty,
@@ -133,7 +131,6 @@ class EtlobhaController extends BaseController
            $input = $request->all();
            $validator =  Validator::make($input ,[
                'name'=>'required|string|max:255',
-              'sku'=>'required|string|unique:products',
               'description'=>'required|string',
             'quantity'=>['required','numeric','gt:0'],
             'less_qty'=>['required','numeric','gt:0'],
@@ -143,7 +140,7 @@ class EtlobhaController extends BaseController
             'cover'=>['nullable','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
             'images'=>'nullable|array',
             'images.*'=>['nullable','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
-              'data'=>'required|array',
+              'data'=>'nullable|array',
               'data.*.type'=>'required|in:brand,color,wight,size',
               'data.*.title'=>'required|string',
               'data.*.value'=>'required|array',
@@ -163,7 +160,6 @@ class EtlobhaController extends BaseController
            }
            $product->update([
               'name' => $request->input('name'),
-              'sku' => $request->input('sku'),
               'description' => $request->input('description'),
               'purchasing_price' => $request->input('purchasing_price'),
               'selling_price' => $request->input('selling_price'),
