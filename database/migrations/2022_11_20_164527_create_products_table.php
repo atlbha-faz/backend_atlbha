0e0ef062e130c2956e5f24c4f8926966bd2f3d42
atlbha <117630530+atlbha@users.noreply.github.com>
2023-04-09 11:46:13 +0300

    Update 2022_11_20_164527_create_products_table.php

--- database/migrations/2022_11_20_164527_create_products_table.php
+++ database/migrations/2022_11_20_164527_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
           $table->id();
             $table->string('name');
-            $table->string('sku')->unique();
+           // $table->string('sku')->unique();
             $table->string('slug');
             $table->enum('for',['store','etlobha','stock'])->default('etlobha');
            $table->enum('special',['special','not_special'])->default('not_special');
