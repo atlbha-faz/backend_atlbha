900dfcf3fb71263d315db3a3174bf194459b7022
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-05-10 11:41:45 +0300

    add index admin

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -23,7 +23,7 @@ class OrderSeeder extends Seeder
             'tax' => 1.2,
             'shipping_price' => 10,
             'discount' => 0,
-            'order_status' => 'new',
+            'order_status' => 'completed',
             'payment_status' => "pending",
             'store_id' => 1,
 
@@ -51,6 +51,45 @@ class OrderSeeder extends Seeder
              'shipping_price' => 10,
             'discount' => 400,
             'order_status' => 'new',
+            'payment_status' => "pending",
+
+        ]);
+         Order::create([
+            'order_number' => '0004',
+            'user_id' => 1,
+            'quantity' => 4,
+            'total_price' => 1000,
+            'tax' => 1.2,
+            'shipping_price' => 10,
+            'discount' => 0,
+            'order_status' => 'new',
+            'payment_status' => "pending",
+            'store_id' => null,
+
+        ]);
+          Order::create([
+            'order_number' => '0005',
+            'user_id' => 2,
+            'quantity' => 5,
+            'total_price' => 1100,
+            'tax' => 2.3,
+            'shipping_price' => 10,
+            'discount' => 400,
+            'order_status' => 'completed',
+            'payment_status' => 'pending',
+            'store_id' => null,
+
+        ]);
+          Order::create([
+            'order_number' => '0006',
+            'user_id' => 6,
+             'store_id' => null,
+            'quantity' => 5,
+            'total_price' => 1100,
+            'tax' => 2.3,
+             'shipping_price' => 10,
+            'discount' => 400,
+            'order_status' => 'completed',
             'payment_status' => "pending",
 
         ]);
