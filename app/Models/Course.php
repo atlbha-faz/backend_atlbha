4f0ed6f36213d8e39a0b3d94adf65f947b6483bc
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-05-14 10:40:01 +0300

    add unit duration

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -10,7 +10,7 @@ class Course extends Model
     use HasFactory;
     protected $fillable = ['name','description','duration','tags','image','link','user_id','status',
     'is_deleted'];
-   
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -30,45 +30,44 @@ class Course extends Model
     return  $videoes;
   }
        public function durationCourse($course_id)
-    {
+   {
         $sum = strtotime('00:00:00');
 
-     $totaltime = 0;
-       $unitid=Unit::select('id')->where('course_id',$course_id)->get();
+        $totaltime = 0;
+        $unitid=Unit::select('id')->where('course_id',$course_id)->get();
 
-    //    $unitid=count($unitid);
+       //    $unitid=count($unitid);
        //$video=Unit::select('id')->where('id',$course_id)->get()
-    $videoes = Video::whereIn('unit_id',$unitid)->get();
-    foreach($videoes as $video){
+       $videoes = Video::whereIn('unit_id',$unitid)->get();
+       foreach($videoes as $video){
            // Converting the time into seconds
-    $timeinsec = strtotime($video->duration) - $sum;
+        $timeinsec = strtotime($video->duration) - $sum;
 
-    // Sum the time with previous value
-    $totaltime = $totaltime + $timeinsec;
-}
+       // Sum the time with previous value
+       $totaltime = $totaltime + $timeinsec;
+          }
 
-// Totaltime is the summation of all
-// time in seconds
+        // Totaltime is the summation of all
+        // time in seconds
 
-// Hours is obtained by dividing
-// totaltime with 3600
-$h = intval($totaltime / 3600);
+        // Hours is obtained by dividing
+        // totaltime with 3600
+        $h = intval($totaltime / 3600);
 
-$totaltime = $totaltime - ($h * 3600);
+        $totaltime = $totaltime - ($h * 3600);
 
-// Minutes is obtained by dividing
-// remaining total time with 60
-$m = intval($totaltime / 60);
+        // Minutes is obtained by dividing
+        // remaining total time with 60
+        $m = intval($totaltime / 60);
 
-// Remaining value is seconds
-$s = $totaltime - ($m * 60);
+        // Remaining value is seconds
+        $s = $totaltime - ($m * 60);
 
-// Printing the result
-return ("$h:$m:$s");
+        // Printing the result
+        return ("$h:$m:$s");
 
 
 
-    return  $durationVideo;
   }
 
 public function setImageAttribute($image)
@@ -90,7 +89,7 @@ public function setImageAttribute($image)
         }
         return asset('storage/images/courses') . '/' . $image;
     }
-    
-    
-    
+
+
+
 }
