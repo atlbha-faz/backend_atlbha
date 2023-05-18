8fe55bf9d316a20ff45e5ce131f4cf35c397050f
atlbha <117630530+atlbha@users.noreply.github.com>
2023-05-18 09:21:12 +0300

    Update ProductResource.php

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -51,9 +51,7 @@ class ProductResource extends JsonResource
             'is_deleted' => $this->is_deleted !== null ? $this->is_deleted : 0,
             'created_at' => (string) $this->created_at,
             'updated_at' => (string) $this->updated_at,
-            'category' => New CategoryResource($this->category),
-            'store' => New StoreResource($this->store),
-         //'images' => ImageResource::collection($this->image),
+         'images' => ImageResource::collection($this->image),
            'options'=>OptionResource::collection($this->option),
 
 
