b2e36ea124fb276e8daff4c057f75dcb64836c30
atlbha <117630530+atlbha@users.noreply.github.com>
2023-05-22 11:02:30 +0300

    Create UsersImport.php

---
+++ app/Imports/UsersImport.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class UsersImport implements ToModel, WithHeadingRow
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new Product([
+            "first_name" => $row['first_name'],
+            'name' => $row['name'],
+            'for' => 'store',
+            'description' => $row['description'],
+            'selling_price' => $row['selling_price'],
+            'stock' => $row['stock'],
+            'cover' => $row['cover'],
+            'SEOdescription'=> $row['SEOdescription'],
+           'discount_price'=>$row['discount_price'],
+            'discount_percent'=>$row['discount_percent'],
+            'subcategory_id' => implode(',', $row['subcategory_id']),
+            'category_id' => $row['category_id'],
+            'store_id'=> auth()->user()->store_id,
+          ]);
+    }
+}
