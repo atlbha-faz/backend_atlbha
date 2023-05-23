b127946c04564a2473ae39b70e9d78a95da8b7c3
atlbha <117630530+atlbha@users.noreply.github.com>
2023-05-23 09:42:25 +0300

    Update ProductsImport.php

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -17,10 +17,10 @@ class ProductsImport implements ToModel
     {
         // dd($row['4']);
             //  dd(Category::where('name',)->pluck('id')->first());
-       // $parent=Category::where('name',$row['category_name'])->pluck('id')->first();
+        $parent=Category::where('name',$row['category_id'])->pluck('id')->first();
         // dd(Category::where('name',$row['7'])->where('parent_id',$parent)->pluck('id')->toArray());
         
-        $sub_categories = explode(',',$row['subcategories']);
+        $sub_categories = explode(',',$row['subcategory_id']);
         return new Product([
 
            'name' => $row['name'],
@@ -28,7 +28,7 @@ class ProductsImport implements ToModel
             'description' => $row['description'],
             'selling_price' => $row['selling_price'],
 
-             'category_id' => Category::where('name',$row['category_name'])->pluck('id')->first(),
+             'category_id' => Category::where('name',$row['category_id'])->pluck('id')->first(),
             // 'cover' => $row['4'],
             'SEOdescription'=> $row['seodescription'],
            'discount_price'=>$row['discount_price'],
