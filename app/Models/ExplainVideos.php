b58e6d4651804d47281cd77f9df3b0609c50a15e
atlbha <117630530+atlbha@users.noreply.github.com>
2023-05-23 12:34:05 +0300

    Update ExplainVideos.php

--- app/Models/ExplainVideos.php
+++ app/Models/ExplainVideos.php
@@ -14,7 +14,7 @@ class ExplainVideos extends Model
         return $this->belongsTo(User::class);
     }
 
-     public function setVideoAttribute($video)
+     /*public function setVideoAttribute($video)
     {
         if (!is_null($video)) {
             if (gettype($video) != 'string') {
@@ -24,7 +24,7 @@ class ExplainVideos extends Model
                 $this->attributes['video'] = $video;
             }
         }
-    }
+    }*/
 
     public function getVideoAttribute($video)
     {
