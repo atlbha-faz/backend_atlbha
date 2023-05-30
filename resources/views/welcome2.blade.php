a6581e77084f8bb9697c4d92c3b7e5e88398be0a
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-05-30 13:38:03 +0300

    add visit page 2

---
+++ resources/views/welcome2.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9TS658YQ9J"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9TS658YQ9J');
+</script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<p>hello</p>
+</body>
+</html>
