0f9381766a14ef8f34f065ac43fef61f5acccfa7
atlbha <117630530+atlbha@users.noreply.github.com>
2023-06-06 17:47:23 +0300

    Update IndexStoreController.php

--- app/Http/Controllers/api/IndexStoreController.php
+++ app/Http/Controllers/api/IndexStoreController.php
@@ -91,7 +91,7 @@ $resent_arrivede_by_category=Category::where('is_deleted',0)->where('store_id',$
         
   $arr=array();
     $offers=DB::table('offers')->where('offers.is_deleted',0)->where('offers.store_id',$id)->join('offers_products', 'offers.id', '=', 'offers_products.offer_id')
-        ->join('offers_products', 'products.id', '=', 'offers_products.product_id')->where('products.store_id',$id)
+        ->join('products', 'products.id', '=', 'offers_products.product_id')->where('products.store_id',$id)
               ->select('products.id')
                  ->groupBy('products.id')->get();
         
