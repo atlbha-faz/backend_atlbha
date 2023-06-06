09c272c4a23d33c143222a7d0fbd69b320d5b157
atlbha <117630530+atlbha@users.noreply.github.com>
2023-06-06 17:02:12 +0300

    Update IndexStoreController.php

--- app/Http/Controllers/api/IndexStoreController.php
+++ app/Http/Controllers/api/IndexStoreController.php
@@ -88,11 +88,9 @@ $resent_arrivede_by_category=Category::where('is_deleted',0)->where('store_id',$
          $success['category']=CategoryResource::collection(Category::where('is_deleted',0)->where('store_id',$id)->with('products')->has('products')->get());
          $success['productsOffers']=Offer::where('is_deleted',0)->where('store_id',$id)->with('products')->has('products')->get();
         $success['productsRatings']=Comment::where('is_deleted',0)->where('store_id',$id)->orderBy('rateing', 'DESC')->with('product')->has('product')->take(3)->get();
-        $productsCategories=Product::where('store_id',$id)->groupBy('category_id')->whereHas('category', function ($query) {
-  $query->where('is_deleted',0);
-})->selectRaw('count(*) as total, category_id')->orderBy('total','DESC')->take(6)->get();
+        $productsCategories=Product::where('store_id',$id)->groupBy('category_id')->selectRaw('count(*) as total, category_id')->orderBy('total','DESC')->take(6)->get();
        foreach( $productsCategories as  $productsCategory){
-        $success['PopularCategories'][]=new CommentResource(Category::where('is_deleted',0)->where('store_id',$id)->where('id', $productsCategory->category_id)->first());
+        $success['PopularCategories'][]=new CommentResource(Category::where('store_id',$id)->where('id', $productsCategory->category_id)->first());
        }
          $success['storeName']=Store::where('is_deleted',0)->where('id',$id)->pluck('store_name')->first();
          $success['storeEmail ']=Store::where('is_deleted',0)->where('id',$id)->pluck('store_email')->first();
