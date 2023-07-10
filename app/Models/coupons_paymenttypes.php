3e220c405da3cd24befd30e7e49650836d91892b
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-07-10 16:01:02 +0300

    update coupons2

--- app/Models/coupons_paymenttypes.php
+++ app/Models/coupons_paymenttypes.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class couponss_paymenttypes extends Model
+class coupons_paymenttypes extends Model
 {
     use HasFactory;
-      protected $table="coupons_paymenttypes";
-    protected $fillable = ['coupon_id','paymenttype_id','type'];
+    protected $table = "coupons_paymenttypes";
+    protected $fillable = ['coupon_id', 'paymenttype_id', 'type'];
     protected $casts = [
         'paymenttype_id' => 'array',
     ];
--- database/migrations/2023_07_10_115125_create_coupons_paymenttypes_table.php
+++ database/migrations/2023_07_10_115125_create_coupons_paymenttypes_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('couponss_paymenttypes', function (Blueprint $table) {
+        Schema::create('coupons_paymenttypes', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('coupon_id')->unsigned();
             $table->foreign('coupon_id')->references('id')->on('coupons')->onDelete('cascade');
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('couponss_paymenttypes');
+        Schema::dropIfExists('coupons_paymenttypes');
     }
 };
