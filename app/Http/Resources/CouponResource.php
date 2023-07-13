d89056f882cde72a23299c2301db57f3ad3ca61d
atlbha <117630530+atlbha@users.noreply.github.com>
2023-07-13 12:10:31 +0300

    Update CouponResource.php

--- app/Http/Resources/CouponResource.php
+++ app/Http/Resources/CouponResource.php
@@ -40,7 +40,7 @@ class CouponResource extends JsonResource
             'free_shipping' => $this->free_shipping !== null ? $this->free_shipping : 0,
             'exception_discount_product' => $this->exception_discount_product !== null ? $this->exception_discount_product : 0,
             'coupon_apply' => $this->coupon_apply,
-            'selected_product' => $this->coupon_apply == "selected_product" ? array_merge($imports->toArray(),$products): null,
+            'selected_product' => $this->coupon_apply == "selected_product" ? array_merge($imports,$products): null,
             'selected_category' => $this->coupon_apply == "selected_category" ? CategoryResource::collection($this->categories) : null,
             'selected_payment' => $this->coupon_apply == "selected_payment" ? PaymenttypeResource::collection($this->paymenttypes) : null,
             'status' => $this->expireCoupon($this->id),
