6a7b8e61455b8a77a68f915ae21e6a4f2e815d4e
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-07-30 15:37:53 +0300

    update subcategory

--- app/Http/Controllers/api/adminDashboard/StoreCategoryController.php
+++ app/Http/Controllers/api/adminDashboard/StoreCategoryController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\api\adminDashboard;
 
+use App\Http\Controllers\api\BaseController as BaseController;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
-use App\Http\Resources\CategoryResource;
 use Illuminate\Support\Facades\Validator;
-use App\Http\Controllers\api\BaseController as BaseController;
+
 class StoreCategoryController extends BaseController
 {
 
@@ -15,7 +16,6 @@ class StoreCategoryController extends BaseController
         $this->middleware('auth:api');
     }
 
-
     /**
      * Display a listing of the resource.
      *
@@ -23,10 +23,10 @@ class StoreCategoryController extends BaseController
      */
     public function index()
     {
-        $success['categories']=CategoryResource::collection(Category::where('is_deleted',0)->where('parent_id',null)->where('for','store')->where('store_id',null)->get());
-        $success['status']= 200;
+        $success['categories'] = CategoryResource::collection(Category::where('is_deleted', 0)->where('parent_id', null)->where('for', 'store')->where('store_id', null)->get());
+        $success['status'] = 200;
 
-         return $this->sendResponse($success,'تم ارجاع جميع التصنيفات بنجاح','categories return successfully');
+        return $this->sendResponse($success, 'تم ارجاع جميع التصنيفات بنجاح', 'categories return successfully');
     }
 
     /**
@@ -48,67 +48,61 @@ class StoreCategoryController extends BaseController
     public function store(Request $request)
     {
 
+        $input = $request->all();
+        $validator = Validator::make($input, [
+            'name' => 'required|string|max:255',
+            'icon' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+            // 'store_id' =>'required|exists:stores,id',
+            'data.*.name' => 'nullable|string|max:255',
+            'data.*.id' => 'nullable|numeric',
 
-            $input = $request->all();
-            $validator =  Validator::make($input ,[
-                'name'=>'required|string|max:255',
-                'icon'=>['required','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
-               // 'store_id' =>'required|exists:stores,id',
-                'data.*.name'=>'nullable|string|max:255',
-                'data.*.id' => 'nullable|numeric',
+        ]);
+        if ($validator->fails()) {
+            return $this->sendError(null, $validator->errors());
+        }
 
-            ]);
-            if ($validator->fails())
-            {
-                return $this->sendError(null,$validator->errors());
-            }
+        $cat = Category::orderBy('id', 'desc')->first();
+        if (is_null($cat)) {
+            $number = 0001;
+        } else {
 
-            $cat=Category::orderBy('id', 'desc')->first();
-          if(is_null($cat)){
-          $number = 0001;
-          }
-          else{
+            $number = $cat->number;
+            $number = ((int) $number) + 1;
+        }
 
-          $number=$cat->number;
-          $number= ((int) $number) +1;
-          }
-
-            $category =Category::create([
-                'name' => $request->name,
-                'number'=> str_pad($number, 4, '0', STR_PAD_LEFT),
-                'icon' => $request->icon,
-                'parent_id'=>null,
-                'for'=>'store',
-                'store_id'=>null,
-              ]);
-
-
-if($request->data){
-
-    foreach($request->data as $data)
-    {
- $cat=Category::orderBy('id', 'desc')->first();
-          $number=$cat->number;
-          $number= ((int) $number) +1;
-
-        $subcategory= new Category([
-            'name' => $data['name'],
+        $category = Category::create([
+            'name' => $request->name,
             'number' => str_pad($number, 4, '0', STR_PAD_LEFT),
-            'parent_id' => $category->id,
-            'for'=> 'store',
-            'store_id'=>null,
-              ]);
+            'icon' => $request->icon,
+            'parent_id' => null,
+            'for' => 'store',
+            'store_id' => null,
+        ]);
 
-        $subcategory->save();
+        if ($request->data) {
 
-    }
-}
+            foreach ($request->data as $data) {
+                $cat = Category::orderBy('id', 'desc')->first();
+                $number = $cat->number;
+                $number = ((int) $number) + 1;
 
+                $subcategory = new Category([
+                    'name' => $data['name'],
+                    'number' => str_pad($number, 4, '0', STR_PAD_LEFT),
+                    'parent_id' => $category->id,
+                    'for' => 'store',
+                    'store_id' => null,
+                ]);
 
-    $success['categories']=New CategoryResource($category);
-    $success['status']= 200;
+                $subcategory->save();
 
-     return $this->sendResponse($success,'تم إضافة التصنيف بنجاح','Category Added successfully');
+            }
+        }
+
+        $success['categories'] = new CategoryResource($category);
+        $success['status'] = 200;
+
+        return $this->sendResponse($success, 'تم إضافة التصنيف بنجاح', 'Category Added successfully');
     }
     /**
      * Display the specified resource.
@@ -118,24 +112,24 @@ if($request->data){
      */
     public function show($category)
     {
-        $category= Category::query()->where('for','store')->where('store_id',null)->find($category);
-        if ( is_null($category) || $category->is_deleted==1){
-               return $this->sendError("القسم غير موجودة","Category is't exists");
-               }
-              $success['categories']=New CategoryResource($category);
-              $success['status']= 200;
+        $category = Category::query()->where('for', 'store')->where('store_id', null)->find($category);
+        if (is_null($category) || $category->is_deleted == 1) {
+            return $this->sendError("القسم غير موجودة", "Category is't exists");
+        }
+        $success['categories'] = new CategoryResource($category);
+        $success['status'] = 200;
 
-               return $this->sendResponse($success,'تم عرض القسم بنجاح','Category showed successfully');
+        return $this->sendResponse($success, 'تم عرض القسم بنجاح', 'Category showed successfully');
     }
 
-     public function children($parnet)
+    public function children($parnet)
     {
-        $category= Category::where('parent_id',$parnet)->where('is_deleted',0)->get();
+        $category = Category::where('parent_id', $parnet)->where('is_deleted', 0)->get();
 
-              $success['categories']=New CategoryResource($category);
-              $success['status']= 200;
+        $success['categories'] = new CategoryResource($category);
+        $success['status'] = 200;
 
-               return $this->sendResponse($success,'تم عرض الاقسام الفرعية بنجاح','sub_Category showed successfully');
+        return $this->sendResponse($success, 'تم عرض الاقسام الفرعية بنجاح', 'sub_Category showed successfully');
     }
 
     /**
@@ -151,19 +145,18 @@ if($request->data){
 
     public function changeStatus($id)
     {
-        $category = Category::query()->where('for','store')->where('store_id',null)->find($id);
-        if (is_null($category) || $category->is_deleted==1){
-         return $this->sendError("القسم غير موجودة","category is't exists");
-         }
-        if($category->status === 'active'){
+        $category = Category::query()->where('for', 'store')->where('store_id', null)->find($id);
+        if (is_null($category) || $category->is_deleted == 1) {
+            return $this->sendError("القسم غير موجودة", "category is't exists");
+        }
+        if ($category->status === 'active') {
             $category->update(['status' => 'not_active']);
-     }
-    else{
-        $category->update(['status' => 'active']);
-    }
-        $success['categories']=New CategoryResource($category);
-        $success['status']= 200;
-         return $this->sendResponse($success,'تم تعدبل حالة القسم بنجاح',' category status updared successfully');
+        } else {
+            $category->update(['status' => 'active']);
+        }
+        $success['categories'] = new CategoryResource($category);
+        $success['status'] = 200;
+        return $this->sendResponse($success, 'تم تعدبل حالة القسم بنجاح', ' category status updared successfully');
 
     }
     /**
@@ -176,83 +169,78 @@ if($request->data){
     public function update(Request $request, $id)
     {
 
-        $category=Category::where('store_id',null)->where('id',$id)->first();
+        $category = Category::where('store_id', null)->where('id', $id)->first();
 
         // dd($request->data);
-      if (is_null($category) ||  $category->is_deleted==1 || $category->for=='etlobha'){
-          return $this->sendError("التصنيف غير موجودة"," Category is't exists");
-     }
-     $category_id=$category->id;
-      // if($request->parent_id == null){
+        if (is_null($category) || $category->is_deleted == 1 || $category->for == 'etlobha') {
+            return $this->sendError("التصنيف غير موجودة", " Category is't exists");
+        }
+        $category_id = $category->id;
+        // if($request->parent_id == null){
 
-          $input = $request->all();
-         $validator =  Validator::make($input ,[
-          'name'=>'required|string|max:255',
-          'data.*.name'=>'nullable|string|max:255',
-          'data.*.id' => 'nullable|numeric',
-          // 'icon'=>['required','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
-         ]);
-         if ($validator->fails())
-         {
-             # code...
-             return $this->sendError(null,$validator->errors());
-         }
-         $category->update([
-             'name' => $request->input('name'),
-           'icon' =>$request->icon,
-              'for' =>'store',
-              'store_id' =>null
-         ]);
+        $input = $request->all();
+        $validator = Validator::make($input, [
+            'name' => 'required|string|max:255',
+            'data.*.name' => 'nullable|string|max:255',
+            'data.*.id' => 'nullable|numeric',
+            // 'icon'=>['required','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
+        ]);
+        if ($validator->fails()) {
+            # code...
+            return $this->sendError(null, $validator->errors());
+        }
+        $category->update([
+            'name' => $request->input('name'),
+            'icon' => $request->icon,
+            'for' => 'store',
+            'store_id' => null,
+        ]);
 
+        $subcategories_id = Category::where('parent_id', $category_id)->pluck('id')->toArray();
 
+        foreach ($subcategories_id as $oid) {
+            if ($request->data != null) {
+                if (!(in_array($oid, array_column($request->data, 'id')))) {
+                    $subcategory = Category::query()->find($oid);
+                    $subcategory->update(['is_deleted' => 1]);
+                }
+            }
+        }
 
-      $subcategories_id = Category::where('parent_id', $category_id)->pluck('id')->toArray();
+        if ($request->data) {
+            foreach ($request->data as $data) {
 
-      foreach ($subcategories_id as $oid) {
-        if($request->data != null){
-      if (!(in_array($oid, array_column($request->data, 'id')))) {
-          $subcategory = Category::query()->find($oid);
-          $subcategory->update(['is_deleted' => 1]);
-          }
-       }
-      }
+                $sub_cat = Category::find($data['id']);
+                if (!is_null($sub_cat)) {
+                    $number = $sub_cat->number;
+                } else {
+                    $cat = Category::orderBy('id', 'desc')->first();
+                    $number = $cat->number;
+                    $number = ((int) $number) + 1;
+                    $number = str_pad($number, 4, '0', STR_PAD_LEFT);
+                }
 
-      if($request->data){
-   foreach ($request->data as $data) {
+                $subcategories[] = Category::updateOrCreate([
+                    'id' => $data['id'],
 
-       $sub_cat = Category::find($data['id']);
- if(!is_null($sub_cat)){
-             $number = $sub_cat->number;
-         }else{
-             $cat=Category::orderBy('id', 'desc')->first();
-          $number=$cat->number;
-          $number= ((int) $number) +1;
-             $number = str_pad($number, 4, '0', STR_PAD_LEFT);
-         }
+                ], [
+                    'name' => $data['name'],
+                    'number' => $number,
+                    'parent_id' => $category_id,
+                    'for' => 'store',
+                    'is_deleted' => 0,
 
-    $subcategories[] = Category::updateOrCreate([
-        'id'=>$data['id'],
+                ]);
+            }
+        } else {
+            $subcategory = Category::where('parent_id', $category_id)->get();
+            foreach ($subcategory as $sub) {$sub->delete();}
+        }
+        $success['categories'] = new CategoryResource($category);
+        $success['status'] = 200;
 
-    ], [
-      'name' => $data['name'],
-       'number'=> $number,
-      'parent_id' => $category_id,
-      'for'=>'store',
-      'is_deleted' => 0,
-
-    ]);
-  }
-  }
-  else{
-    $subcategory = Category::where('parent_id', $category_id)->get();
-    foreach($subcategory as $sub)
-   { $sub->delete();}
-     }        
-         $success['categories']=New CategoryResource($category);
-         $success['status']= 200;
-
-          return $this->sendResponse($success,'تم التعديل بنجاح','Category updated successfully');
-  }
+        return $this->sendResponse($success, 'تم التعديل بنجاح', 'Category updated successfully');
+    }
 
     /**
      * Remove the specified resource from storage.
@@ -262,64 +250,70 @@ if($request->data){
      */
     public function destroy($category)
     {
-        $category =Category::where('store_id',null)->where('id',$category)->first();
-        if (is_null($category) || $category->is_deleted==1){
-            return $this->sendError("القسم غير موجودة","category is't exists");
-            }
-           $category->update(['is_deleted' => 1]);
+        $category = Category::where('store_id', null)->where('id', $category)->first();
+        if (is_null($category) || $category->is_deleted == 1) {
+            return $this->sendError("القسم غير موجودة", "category is't exists");
+        }
+        $category->update(['is_deleted' => 1]);
 
-           $success['activities']=New CategoryResource($category);
-           $success['status']= 200;
-            return $this->sendResponse($success,'تم حذف القسم بنجاح','category deleted successfully');
+        $success['activities'] = new CategoryResource($category);
+        $success['status'] = 200;
+        return $this->sendResponse($success, 'تم حذف القسم بنجاح', 'category deleted successfully');
     }
 
-   public function deleteall(Request $request)
+    public function deleteall(Request $request)
     {
 
-            $categorys =Category::whereIn('id',$request->id)->where('for','store')->where('is_deleted',0)->where('store_id', null)->get();
-             if(count($categorys)>0){
-           foreach($categorys as $category)
-           {
+        $categorys = Category::whereIn('id', $request->id)->where('for', 'store')->where('is_deleted', 0)->where('store_id', null)->get();
+        if (count($categorys) > 0) {
+            foreach ($categorys as $category) {
 
-             $category->update(['is_deleted' => 1]);
-            $success['categorys']=New CategoryResource($category);
+                $category->update(['is_deleted' => 1]);
+                $success['categorys'] = new CategoryResource($category);
 
             }
 
-           $success['status']= 200;
+            $success['status'] = 200;
 
-            return $this->sendResponse($success,'تم حذف التصنيف بنجاح','category deleted successfully');
-          }
-          else{
-            $success['status']= 200;
-        return $this->sendResponse($success,'المدخلات غير صحيحة','id does not exit');
+            return $this->sendResponse($success, 'تم حذف التصنيف بنجاح', 'category deleted successfully');
+        } else {
+            $success['status'] = 200;
+            return $this->sendResponse($success, 'المدخلات غير صحيحة', 'id does not exit');
         }
-        }
-       public function changeSatusall(Request $request)
-            {
+    }
+    public function changeSatusall(Request $request)
+    {
 
-                    $categorys =Category::whereIn('id',$request->id)->where('for','store')->where('store_id', null)->where('is_deleted',0)->get();
-                    if( count($categorys)>0)
-                    {
-                foreach($categorys as $category)
-                {
+        $categorys = Category::whereIn('id', $request->id)->where('for', 'store')->where('store_id', null)->where('is_deleted', 0)->get();
+        if (count($categorys) > 0) {
+            foreach ($categorys as $category) {
 
-                    if($category->status === 'active'){
-                $category->update(['status' => 'not_active']);
+                if ($category->status === 'active') {
+                    $category->update(['status' => 'not_active']);
+                } else {
+                    $category->update(['status' => 'active']);
                 }
-                else{
-                $category->update(['status' => 'active']);
-                }
-                $success['categorys']= New CategoryResource($category);
+                if ($category->parent_id == null) {
+                    $subs = Category::where('parent_id', $category->id)->where('for', 'store')->where('is_deleted', 0)->get();
+                    foreach ($subs as $sub) {
 
+                        if ($sub->status === 'active') {
+                            $sub->update(['status' => 'not_active']);
+                        } else {
+                            $sub->update(['status' => 'active']);
+                        }
                     }
-                    $success['status']= 200;
+                }
 
-                return $this->sendResponse($success,'تم تعديل حالة التصنيف بنجاح','category updated successfully');
-           }
-           else{
-            $success['status']= 200;
-        return $this->sendResponse($success,'المدخلات غير صحيحة','id does not exit');
+                $success['categorys'] = new CategoryResource($category);
+
+            }
+            $success['status'] = 200;
+
+            return $this->sendResponse($success, 'تم تعديل حالة التصنيف بنجاح', 'category updated successfully');
+        } else {
+            $success['status'] = 200;
+            return $this->sendResponse($success, 'المدخلات غير صحيحة', 'id does not exit');
         }
-        }
+    }
 }
