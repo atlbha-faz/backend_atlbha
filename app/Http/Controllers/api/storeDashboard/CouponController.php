ba2a39e4686c68b76fa36bbae7b983ee4f6680b9
atlbha <117630530+atlbha@users.noreply.github.com>
2023-08-02 12:04:53 +0300

    Update CouponController.php

--- app/Http/Controllers/api/storeDashboard/CouponController.php
+++ app/Http/Controllers/api/storeDashboard/CouponController.php
@@ -50,7 +50,9 @@ class CouponController extends BaseController
         // dd($request->free_shipping);
         $input = $request->all();
         $validator = Validator::make($input, [
-            'code' => 'required|regex:/^(?=.*[a-zA-Z])[a-zA-Z0-9]+$/|max:255|unique:coupons',
+            'code' => ['required', 'regex:/^(?=.*[a-zA-Z])[a-zA-Z0-9]+$/','max:255', Rule::unique('coupons')->where(function ($query) {
+                        return $query->where('store_id', auth()->user()->store_id);
+                    })],
             'discount_type' => 'required|in:fixed,percent',
             'total_price' => ['required', 'numeric', 'gt:0'],
             'discount' => ['required', 'numeric', 'gt:0'],
@@ -186,7 +188,10 @@ class CouponController extends BaseController
         }
         $input = $request->all();
         $validator = Validator::make($input, [
-            'code' => ['required', 'regex:/^(?=.*[a-zA-Z])[a-zA-Z0-9]+$/', 'unique:coupons,code,' . $coupon->id],
+
+            'code' => ['required', 'regex:/^(?=.*[a-zA-Z])[a-zA-Z0-9]+$/','max:255', Rule::unique('coupons')->where(function ($query) {
+                        return $query->where('store_id', auth()->user()->store_id)->where('id', '!=', $coupon->id);
+                    })],
             'discount_type' => 'required|in:fixed,percent',
             'total_price' => ['required', 'numeric', 'gt:0'],
             'discount' => ['required', 'numeric', 'gt:0'],
