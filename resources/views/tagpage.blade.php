e062871f5d713bc56b1bb0675664fe5169a343c2
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-08-05 16:28:00 +0300

    update tag google

---
+++ resources/views/tagpage.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-DX60PD756G"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-DX60PD756G');
+    </script>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>tag</title>
+</head>
+
+<body>
+    <p>tag</p>
+</body>
+
+</html>
