75a184b000b794973b5ca3285db0012b40cab60d
atlbha <117630530+atlbha@users.noreply.github.com>
2023-08-20 11:28:30 +0300

    Create SendMail2.php

---
+++ app/Mail/SendMail2.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendMail2 extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {       
+        return $this->from($this->data['store_email'])->subject('E-mail from atlbha.net Contact Form')->view('send_mail_template')->with('data',$this->data);
+
+    }
+}
