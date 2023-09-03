328fa3eb8b96904a9bcfc4711ff83cea2756f28a
atlbha <117630530+atlbha@users.noreply.github.com>
2023-09-03 12:20:28 +0300

    Update TechnicalSupportController.php

--- app/Http/Controllers/api/storeDashboard/TechnicalSupportController.php
+++ app/Http/Controllers/api/storeDashboard/TechnicalSupportController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\api\BaseController as BaseController;
 use App\Http\Resources\TechnicalsupportResource;
 use App\Models\TechnicalSupport;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
