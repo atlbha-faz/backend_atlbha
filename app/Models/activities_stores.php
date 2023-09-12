4b0b55c349dcf563e76e9ae84782ef89ed15857e
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-09-12 14:00:02 +0300

    update role

--- app/Models/activities_stores.php
+++ app/Models/activities_stores.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class activities_stores extends Model
 {
+    
     use HasFactory;
       protected $table="activities_stores";
     protected $fillable = ['activity_id','store_id'];
