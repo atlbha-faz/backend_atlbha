9cb8a4d79928cb51b878aacc239c061f1f57f2f4
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-09-19 12:46:37 +0300

    update category

--- app/Http/Controllers/api/storeDashboard/CategoryController.php
+++ app/Http/Controllers/api/storeDashboard/CategoryController.php
@@ -21,19 +21,28 @@ class CategoryController extends BaseController
      */
     public function index()
     {
-        $success['categories'] = CategoryResource::collection(Category::
-                where('is_deleted', 0)
-                ->where('parent_id', null)
-                ->where('for', 'store')
-                ->where(function ($query) {
-                    $query->where('store_id', auth()->user()->store_id)
-                        ->OrWhere('store_id', null);
-                })->orderByDesc('created_at')->get());
+        $store = auth()->user()->store_id;
+        if ($store->verification_status == "accept") {
+            $success['categories'] = CategoryResource::collection(Category::
+                    where('is_deleted', 0)
+                    ->where('parent_id', null)
+                    ->where('for', 'store')
+                    ->where(function ($query) {
+                        $query->where('store_id', auth()->user()->store_id)
+                            ->OrWhere('store_id', null);
+                    })->orderByDesc('created_at')->get());
 
-        // ->whereIn('store_id', ['', auth()->user()->store_id])->get());
-        $success['status'] = 200;
+            // ->whereIn('store_id', ['', auth()->user()->store_id])->get());
+            $success['status'] = 200;
 
-        return $this->sendResponse($success, 'تم ارجاع جميع التصنيفات بنجاح', 'categories return successfully');
+            return $this->sendResponse($success, 'تم ارجاع جميع التصنيفات بنجاح', 'categories return successfully');
+        } else {
+            $success['categories'] = null;
+            $success['status'] = 200;
+
+            return $this->sendResponse($success, ' لايوجد تصنيفات ', 'no categories');
+
+        }
     }
 
     /**
