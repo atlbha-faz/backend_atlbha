414296fc3700e4507c8ac0e4753c3f33862f3765
atlbha <117630530+atlbha@users.noreply.github.com>
2023-09-19 12:21:45 +0300

    Update Theme.php

--- database/seeders/Theme.php
+++ database/seeders/Theme.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Theme;
+use App\Models\Theme as ThemeModels;
 
 class Theme extends Seeder
 {
