e1a8e33b4a6bf01dd81c4ff00f32c948c2531ae1
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-12-02 12:35:32 +0300

    update special2

--- app/Http/Controllers/api/adminDashboard/ProductController.php
+++ app/Http/Controllers/api/adminDashboard/ProductController.php
@@ -2,15 +2,13 @@
 
 namespace App\Http\Controllers\api\adminDashboard;
 
-use App\Models\Note;
-use App\Models\Image;
-use App\Models\Product;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
+use App\Http\Controllers\api\BaseController as BaseController;
 use App\Http\Resources\NoteResource;
 use App\Http\Resources\ProductResource;
+use App\Models\Note;
+use App\Models\Product;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use App\Http\Controllers\api\BaseController as BaseController;
 
 class ProductController extends BaseController
 {
@@ -27,14 +25,14 @@ class ProductController extends BaseController
      */
     public function index()
     {
-       {
-       $success['products']=ProductResource::collection(Product::with(['store'=> function ($query) {
-    $query->select('id','domain','store_name','store_email');
-},'category'])->where('is_deleted',0)->where('for','store')->orderByDesc('created_at')->select('id','name','status','cover','special','store_id','created_at')->get());
-        $success['status']= 200;
+        {
+            $success['products'] = ProductResource::collection(Product::with(['store' => function ($query) {
+                $query->select('id', 'domain', 'store_name', 'store_email');
+            }, 'category'])->where('is_deleted', 0)->where('for', 'store')->orderByDesc('created_at')->select('id', 'name', 'status', 'cover', 'special', 'store_id', 'created_at', 'admin_special')->get());
+            $success['status'] = 200;
 
-         return $this->sendResponse($success,'تم ارجاع المنتجات بنجاح','products return successfully');
-    }
+            return $this->sendResponse($success, 'تم ارجاع المنتجات بنجاح', 'products return successfully');
+        }
     }
 
     /**
@@ -110,7 +108,6 @@ class ProductController extends BaseController
 //                 }
 //             }
 
-
 //          $success['products']=New ProductResource($product);
 //         $success['status']= 200;
 
@@ -125,21 +122,19 @@ class ProductController extends BaseController
      */
     public function show($product)
     {
-        $product= Product::query()->find($product);
-        if (is_null($product) || $product->is_deleted !=0 || $product->for =='etlobha'){
-               return $this->sendError("المنتج غير موجود","product is't exists");
-               }
-              $success['products']=New ProductResource($product);
-              $success['status']= 200;
+        $product = Product::query()->find($product);
+        if (is_null($product) || $product->is_deleted != 0 || $product->for == 'etlobha') {
+            return $this->sendError("المنتج غير موجود", "product is't exists");
+        }
+        $success['products'] = new ProductResource($product);
+        $success['status'] = 200;
 
-               return $this->sendResponse($success,'تم عرض المنتج بنجاح','product showed successfully');
+        return $this->sendResponse($success, 'تم عرض المنتج بنجاح', 'product showed successfully');
     }
 
-
-
-      public function specialStatusall(Request $request)
+    public function specialStatusall(Request $request)
     {
-      $products = Product::whereIn('id', $request->id)->where('is_deleted', 0)->where('for', 'store')->get();
+        $products = Product::whereIn('id', $request->id)->where('is_deleted', 0)->where('for', 'store')->get();
 
         if (count($products) > 0) {
             foreach ($products as $product) {
@@ -162,49 +157,44 @@ class ProductController extends BaseController
     }
     public function specialStatus($id)
     {
-      $product= Product::where('id', $id)->where('is_deleted', 0)->where('for', 'store')->first();
+        $product = Product::where('id', $id)->where('is_deleted', 0)->where('for', 'store')->first();
 
-      if (is_null($product) || $product->is_deleted !=0 ){
-        return $this->sendError("المنتج غير موجود","product is't exists");
+        if (is_null($product) || $product->is_deleted != 0) {
+            return $this->sendError("المنتج غير موجود", "product is't exists");
         }
 
-                if ($product->admin_special === 'not_special') {
-                    $product->update(['admin_special' => 'special']);
-                } else {
-                    $product->update(['admin_special' => 'not_special']);
-                }
-                $success['product'] = new ProductResource($product);
+        if ($product->admin_special === 'not_special') {
+            $product->update(['admin_special' => 'special']);
+        } else {
+            $product->update(['admin_special' => 'not_special']);
+        }
+        $success['product'] = new ProductResource($product);
 
-
-            $success['status'] = 200;
-            return $this->sendResponse($success, 'تم تعديل حالة المنتج بنجاح', 'Product updated successfully');
-
+        $success['status'] = 200;
+        return $this->sendResponse($success, 'تم تعديل حالة المنتج بنجاح', 'Product updated successfully');
 
     }
 
-       public function changeSatusall(Request $request)
+    public function changeSatusall(Request $request)
     {
 
-            $products =Product::whereIn('id',$request->id)->where('is_deleted',0)->where('for','store')->get();
-            if(count($products)>0){
-           foreach($products as $product)
-           {
-              if($product->status === 'active'){
-            $product->update(['status' => 'not_active']);
-        }
-        else{
-        $product->update(['status' => 'active']);
-        }
-        $success['products']= New ProductResource($product);
+        $products = Product::whereIn('id', $request->id)->where('is_deleted', 0)->where('for', 'store')->get();
+        if (count($products) > 0) {
+            foreach ($products as $product) {
+                if ($product->status === 'active') {
+                    $product->update(['status' => 'not_active']);
+                } else {
+                    $product->update(['status' => 'active']);
+                }
+                $success['products'] = new ProductResource($product);
 
             }
-               $success['status']= 200;
-                 return $this->sendResponse($success,'تم تعديل حالة المنتج بنجاح','product updated successfully');
-    }
-    else{
-        $success['status']= 200;
-    return $this->sendResponse($success,'المدخلات غير صحيحة','id does not exit');
-    }
+            $success['status'] = 200;
+            return $this->sendResponse($success, 'تم تعديل حالة المنتج بنجاح', 'product updated successfully');
+        } else {
+            $success['status'] = 200;
+            return $this->sendResponse($success, 'المدخلات غير صحيحة', 'id does not exit');
+        }
     }
     /**
      * Show the form for editing the specified resource.
@@ -218,15 +208,15 @@ class ProductController extends BaseController
     }
     public function rateing($product)
     {
-        $product =Product::where('is_deleted',0)->where('id',$product)->first();
-        if (is_null($product) ){
-            return $this->sendError("المنتجات غير موجودة"," product is't exists");
+        $product = Product::where('is_deleted', 0)->where('id', $product)->first();
+        if (is_null($product)) {
+            return $this->sendError("المنتجات غير موجودة", " product is't exists");
         }
-        $rating =$product->comment->avg('rateing');
+        $rating = $product->comment->avg('rateing');
 
-        $success['rateing']= $rating;
-        $success['status']= 200;
-         return $this->sendResponse($success,'تم عرض التقييم بنجاح',' rateing showrd successfully');
+        $success['rateing'] = $rating;
+        $success['status'] = 200;
+        return $this->sendResponse($success, 'تم عرض التقييم بنجاح', ' rateing showrd successfully');
 
     }
     /**
@@ -236,7 +226,7 @@ class ProductController extends BaseController
      * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
-   // public function update(Request $request, $id)
+    // public function update(Request $request, $id)
 //   {
 //          $product =Product::query()->find($id);
 //          if (is_null($product) || $product->is_deleted !=0){
@@ -283,7 +273,6 @@ class ProductController extends BaseController
 //             'subcategory_id' => $request->input('subcategory_id'),
 //             'store_id' => $request->input('store_id'),
 
-
 //          ]);
 //          if($request->hasFile("images")){
 //             $files=$request->file("images");
@@ -303,8 +292,6 @@ class ProductController extends BaseController
 //             return $this->sendResponse($success,'تم التعديل بنجاح','product updated successfully');
 //         }
 
-
-
     /**
      * Remove the specified resource from storage.
      *
@@ -314,46 +301,42 @@ class ProductController extends BaseController
     public function deleteall(Request $request)
     {
 
-            $products =Product::whereIn('id',$request->id)->where('is_deleted',0)->where('for','store')->get();
-            if(count($products)>0){
-           foreach($products as $product)
-           {
+        $products = Product::whereIn('id', $request->id)->where('is_deleted', 0)->where('for', 'store')->get();
+        if (count($products) > 0) {
+            foreach ($products as $product) {
 
-               $product->update(['is_deleted' => $product->id]);
+                $product->update(['is_deleted' => $product->id]);
             }
-               $success['products']= ProductResource::collection($products);
-               $success['status']= 200;
-                return $this->sendResponse($success,'تم حذف المنتج بنجاح','product deleted successfully');
-               }
-               else{
-                $success['status']= 200;
-            return $this->sendResponse($success,'المدخلات غير صحيحة','id does not exit');
-            }
-           }
+            $success['products'] = ProductResource::collection($products);
+            $success['status'] = 200;
+            return $this->sendResponse($success, 'تم حذف المنتج بنجاح', 'product deleted successfully');
+        } else {
+            $success['status'] = 200;
+            return $this->sendResponse($success, 'المدخلات غير صحيحة', 'id does not exit');
+        }
+    }
 
-     public function addNote(Request $request)
-     {
+    public function addNote(Request $request)
+    {
         $input = $request->all();
-        $validator =  Validator::make($input ,[
-            'subject'=>'required|string|max:255',
-            'details'=>'required|string',
-            'store_id'=>'required'
+        $validator = Validator::make($input, [
+            'subject' => 'required|string|max:255',
+            'details' => 'required|string',
+            'store_id' => 'required',
         ]);
-        if ($validator->fails())
-        {
-            return $this->sendError(null,$validator->errors());
+        if ($validator->fails()) {
+            return $this->sendError(null, $validator->errors());
         }
         $note = Note::create([
             'subject' => $request->subject,
             'details' => $request->details,
             'store_id' => $request->store_id,
-            'product_id'=>$request->product_id,
-          ]);
+            'product_id' => $request->product_id,
+        ]);
 
+        $success['notes'] = new NoteResource($note);
+        $success['status'] = 200;
 
-         $success['notes']=New NoteResource($note );
-        $success['status']= 200;
-
-         return $this->sendResponse($success,'تم إضافة ملاحظة بنجاح','note Added successfully');
+        return $this->sendResponse($success, 'تم إضافة ملاحظة بنجاح', 'note Added successfully');
     }
 }
