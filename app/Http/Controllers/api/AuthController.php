450fc113eeb12752cec9a51c2f4c2e1121036053
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-12-27 16:46:41 +0300

    update sms

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -203,16 +203,16 @@ class AuthController extends BaseController
                 $user->generateVerifyCode();
                 $request->code = $user->verify_code;
                 $request->phonenumber = $user->phonenumber;
-                // $this->sendSms($request);
-                $data = array(
-                    'code' => $user->verify_code,
-                );
+                $this->sendSms($request);
+                // $data = array(
+                //     'code' => $user->verify_code,
+                // );
 
-                try {
-                    Mail::to($user->email)->send(new SendCode($data));
-                } catch (\Exception $e) {
-                    return $this->sendError('صيغة البريد الالكتروني غير صحيحة', 'The email format is incorrect.');
-                }
+                // try {
+                //     Mail::to($user->email)->send(new SendCode($data));
+                // } catch (\Exception $e) {
+                //     return $this->sendError('صيغة البريد الالكتروني غير صحيحة', 'The email format is incorrect.');
+                // }
 
                 $success['user'] = new UserResource($user);
                 // $success['token'] = $user->createToken('authToken')->accessToken;
@@ -377,16 +377,16 @@ class AuthController extends BaseController
                 $user->generateVerifyCode();
                 $request->code = $user->verify_code;
                 $request->phonenumber = $user->phonenumber;
-                // $this->sendSms($request); // send and return its response
-                $data = array(
-                    'code' => $user->verify_code,
-                );
+                 $this->sendSms($request); // send and return its response
+                // $data = array(
+                //     'code' => $user->verify_code,
+                // );
 
-                try {
-                    Mail::to($user->email)->send(new SendCode($data));
-                } catch (\Exception $e) {
-                    return $this->sendError('صيغة البريد الالكتروني غير صحيحة', 'The email format is incorrect.');
-                }
+                // try {
+                //     Mail::to($user->email)->send(new SendCode($data));
+                // } catch (\Exception $e) {
+                //     return $this->sendError('صيغة البريد الالكتروني غير صحيحة', 'The email format is incorrect.');
+                // }
 
             }
 
