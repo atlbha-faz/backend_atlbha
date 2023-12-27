485ffa1de9f34d2a71d3caf6e8df5f9b013680cf
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-12-27 16:52:49 +0300

    update logo in admin

--- app/Http/Controllers/api/adminDashboard/ProductController.php
+++ app/Http/Controllers/api/adminDashboard/ProductController.php
@@ -34,7 +34,7 @@ class ProductController extends BaseController
     {
         {
             $success['products'] = ProductResource::collection(Product::with(['store' => function ($query) {
-                $query->select('id', 'domain', 'store_name', 'store_email');
+                $query->select('id', 'domain', 'store_name', 'store_email','logo','icon');
             }, 'category'])->where('is_deleted', 0)->where('for', 'store')->orderByDesc('created_at')->select('id', 'name', 'status', 'cover', 'special', 'store_id', 'created_at', 'admin_special')->get());
             $success['status'] = 200;
 
