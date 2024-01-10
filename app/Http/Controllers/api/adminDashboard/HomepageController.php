2f61f01cd48e2af618335babe3deac8d7b1e9888
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2024-01-10 12:24:45 +0300

    update banars and sliders

--- app/Http/Controllers/api/adminDashboard/HomepageController.php
+++ app/Http/Controllers/api/adminDashboard/HomepageController.php
@@ -117,11 +117,11 @@ public function banarUpdate(Request $request)
    }
         $input = $request->all();
        $validator =  Validator::make($input ,[
-        'banar1'=>['nullable','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
+        'banar1'=>['nullable','required_without_all:banar2,banar3' ,'max:2048'],
         'banarstatus1'=>'required|in:active,not_active',
-        'banar2'=>['nullable','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
+        'banar2'=>['nullable','required_without_all:banar3,banar1' ,'max:2048'],
         'banarstatus2'=>'required|in:active,not_active',
-        'banar3'=>['nullable','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
+        'banar3'=>['nullable','required_without_all:banar2,banar1' ,'max:2048'],
         'banarstatus3'=>'required|in:active,not_active',
           ]);
        if ($validator->fails())
@@ -154,11 +154,11 @@ public function sliderUpdate(Request $request)
    }
         $input = $request->all();
        $validator =  Validator::make($input ,[
-        'slider1'=>['nullable','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
+        'slider1'=>['nullable', 'required_without_all:slider2,slider3', 'max:2048'],
         'sliderstatus1'=>'required|in:active,not_active',
-        'slider2'=>['nullable','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
+        'slider2'=>['nullable', 'required_without_all:slider1,slider3' ,'max:2048'],
         'sliderstatus2'=>'required|in:active,not_active',
-        'slider3'=>['nullable','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
+        'slider3'=>['nullable', 'required_without_all:slider2,slider1', 'max:2048'],
         'sliderstatus3'=>'required|in:active,not_active',
           ]);
        if ($validator->fails())
--- app/Models/Homepage.php
+++ app/Models/Homepage.php
@@ -62,10 +62,8 @@ class Homepage extends Model implements Viewable
         if (!is_null($banar1)) {
             if (gettype($banar1) != 'string') {
                 $i = $banar1->store('images/homepage', 'public');
-              
 
                 $this->attributes['banar1'] = $banar1->hashName();
-               
 
             } else {
                 $this->attributes['banar1'] = $banar1;
@@ -78,7 +76,9 @@ class Homepage extends Model implements Viewable
         if (is_null($banar1)) {
             return asset('assets/media/slider.png');
         }
-        return asset('storage/images/homepage') . '/' . $banar1;
+        if (filter_var($banar1, FILTER_VALIDATE_URL)) {
+            return $banar1;} else {
+            return asset('storage/images/homepage') . '/' . $banar1;}
     }
 
     public function setBanar2Attribute($banar2)
@@ -86,10 +86,8 @@ class Homepage extends Model implements Viewable
         if (!is_null($banar2)) {
             if (gettype($banar2) != 'string') {
                 $i = $banar2->store('images/homepage', 'public');
-                
 
                 $this->attributes['banar2'] = $banar2->hashName();
-               
 
             } else {
                 $this->attributes['banar2'] = $banar2;
@@ -102,17 +100,18 @@ class Homepage extends Model implements Viewable
         if (is_null($banar2)) {
             return asset('assets/media/slider.png');
         }
-        return asset('storage/images/homepage') . '/' . $banar2;
+        if (filter_var($banar2, FILTER_VALIDATE_URL)) {
+            return $logbanar2o;} else {
+            return asset('storage/images/homepage') . '/' . $banar2;
+        }
     }
     public function setBanar3Attribute($banar3)
     {
         if (!is_null($banar3)) {
             if (gettype($banar3) != 'string') {
                 $i = $banar3->store('images/homepage', 'public');
-               
 
                 $this->attributes['banar3'] = $banar3->hashName();
-              
 
             } else {
                 $this->attributes['banar3'] = $banar3;
@@ -125,16 +124,18 @@ class Homepage extends Model implements Viewable
         if (is_null($banar3)) {
             return asset('assets/media/slider.png');
         }
-        return asset('storage/images/homepage') . '/' . $banar3;
+        if (filter_var($banar3, FILTER_VALIDATE_URL)) {
+            return $banar3;} else {
+            return asset('storage/images/homepage') . '/' . $banar3;
+        }
     }
     public function setSlider1Attribute($slider1)
     {
         if (!is_null($slider1)) {
             if (gettype($slider1) != 'string') {
                 $i = $slider1->store('images/homepage', 'public');
-              
+
                 $this->attributes['slider1'] = $slider1->hashName();
-           
 
             } else {
                 $this->attributes['slider1'] = $slider1;
@@ -147,17 +148,18 @@ class Homepage extends Model implements Viewable
         if (is_null($slider1)) {
             return asset('assets/media/slider.png');
         }
-        return asset('storage/images/homepage') . '/' . $slider1;
+        if (filter_var($slider1, FILTER_VALIDATE_URL)) {
+            return $slider1;} else {
+            return asset('storage/images/homepage') . '/' . $slider1;
+        }
     }
     public function setSlider2Attribute($slider2)
     {
         if (!is_null($slider2)) {
             if (gettype($slider2) != 'string') {
                 $i = $slider2->store('images/homepage', 'public');
-                ;
 
                 $this->attributes['slider2'] = $slider2->hashName();
-              
 
             } else {
                 $this->attributes['slider2'] = $slider2;
@@ -170,17 +172,18 @@ class Homepage extends Model implements Viewable
         if (is_null($slider2)) {
             return asset('assets/media/slider.png');
         }
-        return asset('storage/images/homepage') . '/' . $slider2;
+        if (filter_var($slider2, FILTER_VALIDATE_URL)) {
+            return $slider2;} else {
+            return asset('storage/images/homepage') . '/' . $slider2;
+        }
     }
     public function setSlider3Attribute($slider3)
     {
         if (!is_null($slider3)) {
             if (gettype($slider3) != 'string') {
                 $i = $slider3->store('images/homepage', 'public');
-            
 
                 $this->attributes['slider3'] = $slider3->hashName();
-               
 
             } else {
                 $this->attributes['slider3'] = $slider3;
@@ -193,6 +196,9 @@ class Homepage extends Model implements Viewable
         if (is_null($slider3)) {
             return asset('assets/media/slider.png');
         }
-        return asset('storage/images/homepage') . '/' . $slider3;
+        if (filter_var($slider3, FILTER_VALIDATE_URL)) {
+            return $slider3;} else {
+            return asset('storage/images/homepage') . '/' . $slider3;
+        }
     }
 }
