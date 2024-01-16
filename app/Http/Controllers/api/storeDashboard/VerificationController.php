5a712f228f2e57cee9f58608eb2021a6c4ce7fce
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-01-16 14:21:50 +0300

    update

--- app/Http/Controllers/api/storeDashboard/VerificationController.php
+++ app/Http/Controllers/api/storeDashboard/VerificationController.php
@@ -106,7 +106,8 @@ class VerificationController extends BaseController
         $users = User::where('store_id', null)->whereIn('user_type', ['admin', 'admin_employee'])->whereIn('id',[1,2])->get();
                       
         $data = [
-            'message' => 'https://admin.atlbha.com/verification'.$store->owner_name.'طلب توثيق من ',
+            'message' => ' https://admin.atlbha.com/verification  '
+            .' طلب توثيق من '.$store->owner_name,
             'store_id' =>  $store->id,
             'user_id' => auth()->user()->id,
             'type' => "طلب توثيق",
