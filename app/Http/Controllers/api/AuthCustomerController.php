85248b1eabeb87e5a1fb2e051a8d2666aeed95bc
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-01-24 13:31:26 +0300

    update

--- app/Http/Controllers/api/AuthCustomerController.php
+++ app/Http/Controllers/api/AuthCustomerController.php
@@ -343,18 +343,18 @@ class AuthCustomerController extends BaseController
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://el.cloud.unifonic.com/rest/SMS/messages?AppSid=3x6ZYsW1gCpWwcCoMhT9a1Cj1a6JVz&Body='. $request->code .' &Recipient='. $request->phonenumber,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
+          CURLOPT_URL => 'https://el.cloud.unifonic.com/rest/SMS/messages?AppSid=3x6ZYsW1gCpWwcCoMhT9a1Cj1a6JVz&Body='. $request->code .' &Recipient='. $request->phonenumber,
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'POST',
         ));
-
+        
         $response = curl_exec($curl);
-
+        
         curl_close($curl);
        
         $responseData = json_decode( $response);
