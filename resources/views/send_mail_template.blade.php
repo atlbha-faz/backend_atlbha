f2edfabf63ac64d4bc0ada383e4549d7c6edc369
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-01-25 11:45:38 +0300

    update

--- resources/views/send_mail_template.blade.php
+++ resources/views/send_mail_template.blade.php
@@ -3,8 +3,10 @@
 <head>
 <meta charset="utf-8">
 </head>
+<body dir="rtl">
 @isset($data['type'])
 <h2>الموضوع:	{{$data['type']}}</h2>
 @endisset
 <h2>الرسالة:	{{$data['message']}}</h2>
+</body>
 </html>
