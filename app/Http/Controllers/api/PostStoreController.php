a009b90c1c8ce6b43777c217452a6e651e3b93cd
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-01-29 12:54:13 +0300

    update

--- app/Http/Controllers/api/PostStoreController.php
+++ app/Http/Controllers/api/PostStoreController.php
@@ -356,7 +356,8 @@ class PostStoreController extends BaseController
                 $success['pages'] = PageResource::collection(Page::where('is_deleted', 0)->where('status', 'active')->where('store_id', $store_id)->where('postcategory_id', null)->get());
                 // $pages=Page_page_category::where('page_category_id',2)->pluck('page_id')->toArray();
                 $success['postCategory'] = Postcategory::where('is_deleted', 0)->get();
-                $success['lastPosts'] = PageResource::collection(Page::where('is_deleted', 0)->where('status', 'active')->where('store_id', $store_id)->where('postcategory_id', '!=', null)->orderBy('created_at', 'desc')->take(3)->get());
+                $cats = Page_page_category::where('page_category_id',1)->pluck('page_id')->toArray();
+                $success['lastPosts'] = PageResource::collection(Page::where('is_deleted', 0)->where('status', 'active')->where('store_id', $store_id)->where('id',$cats)->orderBy('created_at', 'desc')->take(3)->get());
                 $success['relatedPosts'] = PageResource::collection(Page::where('is_deleted', 0)->where('status', 'active')->where('store_id', $store_id)->where('postcategory_id', '!=', null)->where('postcategory_id', '==', Page::find($pageId)->postcategory_id)->orderBy('created_at', 'desc')->take(2)->get());
                 // $success['footer']=PageResource::collection(Page::where('is_deleted',0)->whereIn('id',$pages)->get());
                 // footer
