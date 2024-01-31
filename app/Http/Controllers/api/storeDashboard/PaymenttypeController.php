2a635fb71bb673a2317efd30c311e27e21f38c7c
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-01-31 11:44:46 +0300

    supplier

--- app/Http/Controllers/api/storeDashboard/PaymenttypeController.php
+++ app/Http/Controllers/api/storeDashboard/PaymenttypeController.php
@@ -28,52 +28,7 @@ class PaymenttypeController extends BaseController
         return $this->sendResponse($success, 'تم ارجاع طرق الدفع بنجاح', 'payment types return successfully');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-//     public function store(Request $request)
-//    {
-//         $input = $request->all();
-//         $validator =  Validator::make($input ,[
-//             'name'=>'required|string|max:255',
-//             'image'=>['required','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
-
-//         ]);
-//         if ($validator->fails())
-//          {
-//             return $this->sendError(null,$validator->errors());
-//          }
-//          $paymenttype = Paymenttype::create([
-//             'name' => $request->name,
-//             'image' => $request->image,
-
-//           ]);
-
-//          $success['paymenttypes']=New PaymenttypeResource($paymenttype);
-//          $success['status']= 200;
-
-//           return $this->sendResponse($success,'تم إضافة طرق دفع بنجاح',' Payment type Added successfully');
-//     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Paymenttype  $paymenttype
-     * @return \Illuminate\Http\Response
-     */
     public function show($paymenttype)
     {
         $paymenttype = Paymenttype::query()->find($paymenttype);
@@ -98,40 +53,7 @@ class PaymenttypeController extends BaseController
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Paymenttype  $paymenttype
-     * @return \Illuminate\Http\Response
-     */
-    // public function update(Request $request, Paymenttype $paymenttype)
-    //  {
-    //      if (is_null($paymenttype) || $paymenttype->is_deleted !=0){
-    //      return $this->sendError("طريقة الدفع غير موجودة","payment type is't exists");
-    //       }
-    //      $input = $request->all();
-    //      $validator =  Validator::make($input ,[
-    //        'name'=>'required|string|max:255',
-    //         'image'=>['required','image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
-    //      ]);
-    //      if ($validator->fails())
-    //      {
-    //         # code...
-    //         return $this->sendError(null,$validator->errors());
-    //      }
-    //      $paymenttype->update([
-    //         'name' => $request->input('name'),
-
-    //     'image' => $request->input('image'),
-
-    //      ]);
-    //      //$country->fill($request->post())->update();
-    //         $success['paymenttypes']=New PaymenttypeResource($paymenttype);
-    //         $success['status']= 200;
-
-    //         return $this->sendResponse($success,'تم التعديل بنجاح','payment type updated successfully');
-    // }
+ 
 
     public function changeStatus($id)
     {
@@ -159,24 +81,7 @@ class PaymenttypeController extends BaseController
 
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Paymenttype  $paymenttype
-     * @return \Illuminate\Http\Response
-     */
-    // public function destroy($paymenttype)
-    //   {
-    //    $paymenttype = Paymenttype::query()->find($paymenttype);
-    //      if (is_null($paymenttype) || $paymenttype->is_deleted !=0){
-    //      return $this->sendError("طريقةالدفع غير موجودة","payment type is't exists");
-    //      }
-    //     $paymenttype->update(['is_deleted' => 1]);
 
-    //     $success['paymenttypes']=New PaymenttypeResource($paymenttype);
-    //     $success['status']= 200;
-    //
 
-    //      return $this->sendResponse($success,'تم حذف طريقةالدفع بنجاح','payment type deleted successfully');
-    // }
+
 }
--- app/Services/FatoorahServices.php
+++ app/Services/FatoorahServices.php
@@ -1,23 +1,25 @@
 <?php
 namespace App\Services;
+
 // use App\ModelTax;
 // use GuzzleHttp\Client;
-use Illuminate\Database\Eloguent\Model;
+
 // use GuzzleHttp\Psr7\Request;
-class FatoorahServices{
+class FatoorahServices
+{
     private $base_url;
     private $token;
     private $headers;
- 
 
-    public function __construct(){
+    public function __construct()
+    {
 
-       $this->base_url =env('fatoora_base_url','https://apitest.myfatoorah.com/');
-       $this->token =env("fatoora_token","rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL");
-          $this->headers= [
-           "Content-Type" =>'application/json',
-           "authorization" => 'Bearer '.env("fatoora_token","rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL")
-       ];
+        $this->base_url = env('fatoora_base_url', 'https://apitest.myfatoorah.com/');
+        $this->token = env("fatoora_token", "rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL");
+        $this->headers = [
+            "Content-Type" => 'application/json',
+            "authorization" => 'Bearer ' . env("fatoora_token", "rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL"),
+        ];
     }
 
     // public function buildRequest($url,$mothod, $data =[]){
@@ -40,80 +42,23 @@ class FatoorahServices{
     //     return $response;
     // }
 
-     public function callAPI($endpointURL, $apiKey, $postFields = []) {
+    public function callAPI($endpointURL, $apiKey, $postFields = [])
+    {
         $curl = curl_init($endpointURL);
         curl_setopt_array($curl, array(
-            CURLOPT_CUSTOMREQUEST  => 'POST',
-            CURLOPT_POSTFIELDS     => json_encode($postFields),
-            CURLOPT_HTTPHEADER     => array("Authorization: Bearer $apiKey", 'Content-Type: application/json'),
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => json_encode($postFields),
+            CURLOPT_HTTPHEADER => array("Authorization: Bearer $apiKey", 'Content-Type: application/json'),
             CURLOPT_RETURNTRANSFER => true,
         ));
         $response = curl_exec($curl);
-        $curlErr  = curl_error($curl);
+        $curlErr = curl_error($curl);
         curl_close($curl);
         return $response;
     }
-    public function getBank($url) {
-        $endpointURL=$this->base_url.$url;
-        $curl = curl_init($endpointURL);
-        curl_setopt_array($curl, array(
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => '',
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 0,
-          CURLOPT_FOLLOWLOCATION => true,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => 'GET',
-          CURLOPT_HTTPHEADER => array("Authorization: Bearer $this->token", 'Content-Type: application/json'),
-        ));
-        
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return json_decode($response);
-        
-    }
-    public function createSupplier($url,$data) {
-        $endpointURL=$this->base_url.$url;
-        $curl = curl_init($endpointURL);
-        curl_setopt_array($curl, array(
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => $data,
-          CURLOPT_HTTPHEADER => array("Authorization: Bearer $this->token", 'Content-Type: application/json'),
-        ));
-        
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return json_decode($response);
-        
-    }
-    public function executePayment($url,$data) {
-        $endpointURL=$this->base_url.$url;
-        $curl = curl_init($endpointURL);
-        curl_setopt_array($curl, array(
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => $data,
-          CURLOPT_HTTPHEADER => array("Authorization: Bearer $this->token", 'Content-Type: application/json'),
-        ));
-        
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return json_decode($response);
-        
-    }
-    public function getSupplierDashboard($url) {
-        $endpointURL=$this->base_url.$url;
+    public function getBank($url)
+    {
+        $endpointURL = $this->base_url . $url;
         $curl = curl_init($endpointURL);
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => true,
@@ -123,17 +68,80 @@ class FatoorahServices{
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'GET',
-          CURLOPT_HTTPHEADER => array("Authorization: Bearer $this->token", 'Content-Type: application/json'),
+            CURLOPT_HTTPHEADER => array("Authorization: Bearer $this->token", 'Content-Type: application/json'),
         ));
-        
+
         $response = curl_exec($curl);
         curl_close($curl);
         return json_decode($response);
-        
-    }
-    
 
+    }
+    public function createSupplier($url, $data)
+    {
+         $endpointURL=$this->base_url.$url;
+        $curl = curl_init($endpointURL);
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>$data,
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer '.$this->token
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return json_decode($response);
+
+    }
+    public function executePayment($url, $data)
+    {
+        $endpointURL = $this->base_url . $url;
+        $curl = curl_init($endpointURL);
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $data,
+            CURLOPT_HTTPHEADER => array("Authorization: Bearer $this->token", 'Content-Type: application/json'),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($response);
+
+    }
+    public function getSupplierDashboard($url)
+    {
+        $endpointURL = $this->base_url . $url;
+        $curl = curl_init($endpointURL);
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer '.$this->token
+            ),
+                ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($response);
+
+    }
 
 }
-
-?>
