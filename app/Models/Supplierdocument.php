cc82c8056b4980512b7d66cf9d632e55b97771d3
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-02-10 15:05:26 +0300

    update

--- app/Models/Supplierdocument.php
+++ app/Models/Supplierdocument.php
@@ -10,22 +10,22 @@ class Supplierdocument extends Model
     use HasFactory;
     protected $fillable = ['file','type','desc','supplierCode','store_id'];
     
-    public function setFileAttribute($file)
-    {
-        if (!is_null($file)) {
-            if (gettype($file) != 'string') {
-                $i = $file->store('images/storelogo', 'public');
-                $this->attributes['file'] = $file->hashName();
-            } else {
-                $this->attributes['file'] = $file;
-            }
-        }
-    }
-    public function getFileAttribute($file)
-    {
-        if (is_null($file)) {
-            return asset('assets/media/man.png');
-        }
-        return asset('storage/images/storelogo') . '/' . $file;
-    }
+    // public function setFileAttribute($file)
+    // {
+    //     if (!is_null($file)) {
+    //         if (gettype($file) != 'string') {
+    //             $i = $file->store('images/storelogo', 'public');
+    //             $this->attributes['file'] = $file->hashName();
+    //         } else {
+    //             $this->attributes['file'] = $file;
+    //         }
+    //     }
+    // }
+    // public function getFileAttribute($file)
+    // {
+    //     if (is_null($file)) {
+    //         return asset('assets/media/man.png');
+    //     }
+    //     return asset('storage/images/storelogo') . '/' . $file;
+    // }
 }
