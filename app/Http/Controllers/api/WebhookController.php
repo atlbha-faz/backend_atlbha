a3761e7991ea7f0f38c1142222af15e5b3c36312
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-03-05 18:43:18 +0300

    update

--- app/Http/Controllers/api/WebhookController.php
+++ app/Http/Controllers/api/WebhookController.php
@@ -1,17 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\api;
 
 use Illuminate\Http\Request;
-
-class WebhookController extends Controller
+use App\Http\Controllers\api\BaseController as BaseController;
+class WebhookController extends BaseController
 {
-    public function handleWebhook(Request $request)
-    {
-        $eventName = $request->input('event');
-        
-    }
-    public function verify($body, $secret, $MyFatoorah_Signature) {
+   
+    public function validateSignature($body, $secret, $MyFatoorah_Signature) {
 
             if ($body['Event'] == 'RefundStatusChanged') {
                 unset($body['Data']['GatewayReference']);
@@ -57,6 +53,21 @@ class WebhookController extends Controller
                 error_log(PHP_EOL . date('d.m.Y h:i:s') . ' - Signature is not valid ', 3, './webhook.log');
                 exit;
             }
+        }
+        public function handleWebhook(Request $request)
+        {
+            $data=$request->get('Data');
+            dd($data);
+            $secret="WVjz6cCoT9N7ZlqmZBMPANjkh30UvH9uwB20g-xGJi8tOjADM9NWiKaasfQCwkGLBy0ZNzoRXYCwMoLzaNwNgvY9H_3pO0bfTfwcEBCHe4s7FDQ0oGYFdMj8UwECAoiV4_3buuymrCmvdzc6QmZZuPsNfFCPg0vtwanErRHxM975FhaReP5QZsp6cU5bE8zupH5qOL7y8Hb9kSTW4u4ffx5V0WqUTrL2GmBWmAhx4eZBqoppO-jxG93E_FU7drhRA8SxiH__pNDDj3RJXBFqbLjzLjQ0iLtvR4s-c7X_dcLWXCj0X5lCBxVPFquo7Fsosbv5-NHJ-8nygVqy-HhwqnN3CV5HRD005E34zf32K8Y0eC526P2wZer5U-jr275rPEtfotn2wFFuDcWWnuT5f37p7oLDOgb2BclrmSj5crn5BxtkbG7awbxR7yVXoW-q19oE6-mcQWoNX8vdSbbdJ8arugLsR6qKW15juYZ8LztWfwnq65rRPZdh_JuE3KO96_rQR72a90FXy0mxjuXWmQU94Nek-2X_9DyecBqPxANjFwotZRNybG353CZchyvyJ60WjhVlfmxLMCdTD6wsBB5Ew5xKNru_jdG5TsshtNUgiogmf4FvJ0M8R3Xlxv98z5VXkqYytzBEtk2rbCwFopar9Ejj2Dwun5YaT5xyllcXrJltQ4UwofJ9j-bislP57_wQZCSachFOs2BaXqnRJEMb8sf6QeRm06TV-F4x7-iGJ-z7";
+            $signature = $request->header('MyFatoorah-Signature');
+            if(!$this->validateSignature($data,$secret,$signature )){
+                return;
+            }
+            $event= $request->input('EventType');
+            // if( $event == 1){
+
+            // }
+            
         }
     
 }
