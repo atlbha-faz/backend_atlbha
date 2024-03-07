96892cbca634530222168a23a01188546d89380a
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-03-07 11:59:00 +0300

    update

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -9,7 +9,11 @@ class Payment extends Model
 {
     use HasFactory;
     protected $fillable = ['paymenDate','paymentType','paymentTransectionID','paymentCardID','deduction','price_after_deduction','orderID','store_id'];
-   
+    protected $casts = [
+        'deduction' => 'float',
+        'price_after_deduction' => 'float',
+    
+    ];
     public function order()
     {
         return $this->belongsTo(Order::class, 'orderID', 'id');
--- database/migrations/2023_02_28_103450_create_payments_table.php
+++ database/migrations/2023_02_28_103450_create_payments_table.php
@@ -19,8 +19,8 @@ return new class extends Migration
             $table->string('paymentType')->nullable();
             $table->string('paymentTransectionID')->nullable();
             $table->string('paymentCardID')->nullable();
-            $table->integer('deduction')->nullable();
-            $table->integer('price_after_deduction')->nullable();
+            $table->double('deduction')->nullable();
+            $table->double('price_after_deduction')->nullable();
             $table->integer('default_option')->nullable();
             $table->unsignedBigInteger('orderID')->nullable();
             $table->foreign('orderID')->references('id')->on('orders')->onDelete('cascade');
