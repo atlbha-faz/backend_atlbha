bcc7c39b0732cc1e11a76a911c7f2a157c82468c
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-03-19 15:21:49 +0300

    update

--- app/Http/Controllers/api/adminDashboard/ActivityController.php
+++ app/Http/Controllers/api/adminDashboard/ActivityController.php
@@ -2,22 +2,20 @@
 
 namespace App\Http\Controllers\api\adminDashboard;
 
+use App\Http\Controllers\api\BaseController as BaseController;
+use App\Http\Resources\ActivityResource;
 use App\Models\Activity;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
-use App\Http\Resources\ActivityResource;
 use Illuminate\Support\Facades\Validator;
-use App\Http\Controllers\api\BaseController as BaseController;
+use Illuminate\Validation\Rule;
 
 class ActivityController extends BaseController
 {
-
 
     public function __construct()
     {
         $this->middleware('auth:api');
     }
-
 
     /**
      * Display a listing of the resource.
@@ -26,10 +24,10 @@ class ActivityController extends BaseController
      */
     public function index()
     {
-        $success['activities']=ActivityResource::collection(Activity::where('is_deleted',0)->orderByDesc('created_at')->get());
-        $success['status']= 200;
+        $success['activities'] = ActivityResource::collection(Activity::where('is_deleted', 0)->orderByDesc('created_at')->get());
+        $success['status'] = 200;
 
-         return $this->sendResponse($success,'تم ارجاع جميع الانشطة بنجاح','Activities return successfully');
+        return $this->sendResponse($success, 'تم ارجاع جميع الانشطة بنجاح', 'Activities return successfully');
     }
 
     /**
@@ -51,27 +49,25 @@ class ActivityController extends BaseController
     public function store(Request $request)
     {
         $input = $request->all();
-        $validator =  Validator::make($input ,[
-            'name'=>['required','string','max:255',Rule::unique('activities')->where(function ($query) {
+        $validator = Validator::make($input, [
+            'name' => ['required', 'string', 'max:255', Rule::unique('activities')->where(function ($query) {
                 return $query->where('is_deleted', 0);
             })],
-                'icon'=>['image','mimes:jpeg,png,jpg,gif,svg','max:1048'],
+            'icon' => ['image', 'mimes:jpeg,png,jpg,gif,svg', 'max:1048'],
         ]);
-        if ($validator->fails())
-        {
-            return $this->sendError(null,$validator->errors());
+        if ($validator->fails()) {
+            return $this->sendError(null, $validator->errors());
         }
         $activity = Activity::create([
             'name' => $request->name,
             'icon' => $request->icon,
-            
-          ]);
 
+        ]);
 
-         $success['activities']=New ActivityResource($activity );
-        $success['status']= 200;
+        $success['activities'] = new ActivityResource($activity);
+        $success['status'] = 200;
 
-         return $this->sendResponse($success,'تم إضافة النشاط بنجاح','Activity Added successfully');
+        return $this->sendResponse($success, 'تم إضافة النشاط بنجاح', 'Activity Added successfully');
     }
 
     /**
@@ -80,8 +76,6 @@ class ActivityController extends BaseController
      * @param  \App\Models\Activity  $activity
      * @return \Illuminate\Http\Response
      */
-
-
 
     /**
      * Show the form for editing the specified resource.
@@ -105,32 +99,31 @@ class ActivityController extends BaseController
     {
         $activity = Activity::where('id', $activity)->first();
 
-        if ( is_null($activity) || $activity->is_deleted !=0){
-            return $this->sendError("النشاط غير موجودة"," Activity is't exists");
-       }
-            $input = $request->all();
-           $validator =  Validator::make($input ,[
-                'name'=>['required','string','max:255',Rule::unique('activities')->where(function ($query) use ($activity) {
-                    return $query->where('is_deleted', 0)->where('id', '!=', $activity->id);
-                })],
-                'icon'=>['image','mimes:jpeg,png,jpg,gif,svg','max:1048'],
+        if (is_null($activity) || $activity->is_deleted != 0) {
+            return $this->sendError("النشاط غير موجودة", " Activity is't exists");
+        }
+        $input = $request->all();
+        $validator = Validator::make($input, [
+            'name' => ['required', 'string', 'max:255', Rule::unique('activities')->where(function ($query) use ($activity) {
+                return $query->where('is_deleted', 0)->where('id', '!=', $activity->id);
+            })],
+            'icon' => ['image', 'mimes:jpeg,png,jpg,gif,svg', 'max:1048'],
 
-           ]);
-           if ($validator->fails())
-           {
-               # code...
-               return $this->sendError(null,$validator->errors());
-           }
-           $activity->update([
-               'name' => $request->input('name'),
-                'icon' =>$request->icon,
+        ]);
+        if ($validator->fails()) {
+            # code...
+            return $this->sendError(null, $validator->errors());
+        }
+        $activity->update([
+            'name' => $request->input('name'),
+            'icon' => $request->icon,
 
-           ]);
+        ]);
 
-           $success['activities']=New ActivityResource($activity);
-           $success['status']= 200;
+        $success['activities'] = new ActivityResource($activity);
+        $success['status'] = 200;
 
-            return $this->sendResponse($success,'تم التعديل بنجاح','Activity updated successfully');
+        return $this->sendResponse($success, 'تم التعديل بنجاح', 'Activity updated successfully');
     }
 
     /**
@@ -139,27 +132,23 @@ class ActivityController extends BaseController
      * @param  \App\Models\Activity  $activity
      * @return \Illuminate\Http\Response
      */
- public function deleteall(Request $request)
+    public function deleteall(Request $request)
     {
 
+        $activities = Activity::whereIn('id', $request->id)->where('is_deleted', 0)->get();
+        if (count($activities) > 0) {
+            foreach ($activities as $activity) {
 
-            $activities =Activity::whereIn('id',$request->id)->where('is_deleted',0)->get();
-            if(count($activities)>0){
-           foreach($activities as $activity)
-           {
-        
-               $activity->update(['is_deleted' => $activity->id]);
-              $success['activities']= New ActivityResource($activity);
+                $activity->update(['is_deleted' => $activity->id]);
+                $success['activities'] = new ActivityResource($activity);
 
             }
-               $success['status']= 200;
-                return $this->sendResponse($success,'تم حذف الأنشطة بنجاح','Activity deleted successfully');
-           }
-
-      else{
-          $success['status']= 200;
-       return $this->sendResponse($success,'المدخلات غيرموجودة','id is not exit');
+            $success['status'] = 200;
+            return $this->sendResponse($success, 'تم حذف الأنشطة بنجاح', 'Activity deleted successfully');
+        } else {
+            $success['status'] = 200;
+            return $this->sendResponse($success, 'المدخلات غيرموجودة', 'id is not exit');
         }
 
-     }
+    }
 }
