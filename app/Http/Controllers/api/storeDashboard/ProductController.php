b71d30e0cdca51fa18d6aafbab172b3da59678d7
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-03-26 10:26:50 +0300

    update

--- app/Http/Controllers/api/storeDashboard/ProductController.php
+++ app/Http/Controllers/api/storeDashboard/ProductController.php
@@ -39,7 +39,7 @@ class ProductController extends BaseController
             $query->select('id', 'domain', 'store_name');
         }, 'category' => function ($query) {
             $query->select('id', 'name');}])
-                ->where('is_deleted', 0)->where('store_id', auth()->user()->store_id)->where('for', 'store')->orderByDesc('created_at')->select('id', 'name', 'status', 'cover', 'special', 'store_id', 'created_at', 'category_id', 'subcategory_id', 'selling_price', 'purchasing_price', 'discount_price', 'stock', 'description','is_import','original_id','short_description')->paginate(15)
+                ->where('is_deleted', 0)->where('store_id', auth()->user()->store_id)->where('for', 'store')->orderByDesc('created_at')->select('id', 'name', 'status', 'cover', 'special', 'store_id', 'created_at', 'category_id', 'subcategory_id', 'selling_price', 'purchasing_price', 'discount_price', 'stock', 'description','is_import','original_id','short_description')->paginate(10)
         );
 
         // $import = Product::join('importproducts', 'products.id', '=', 'importproducts.product_id')->where('products.is_deleted', 0)->where('importproducts.store_id', auth()->user()->store_id)
