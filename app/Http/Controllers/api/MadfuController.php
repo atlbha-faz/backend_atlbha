f12c33c7543a19e8f59de6fb34eedc564ed102b2
ahmed <aattya@goatar.com>
2024-04-18 15:23:48 +0200

    Madfu login

--- app/Http/Controllers/api/MadfuController.php
+++ app/Http/Controllers/api/MadfuController.php
@@ -15,7 +15,8 @@ class MadfuController extends BaseController
         $password = 'QU1NTAUNS1NXSSE';
         $login_request = (new Madfu())->login($username, $password, $request->uuid);
         if ($login_request->getStatusCode() == 200) {
-            return $this->sendResponse(['token' => json_decode($login_request->getBody()->getContents())->token], 'عملية ناجحة', 'Success process');
+            return $this->sendResponse(['token' => json_decode($login_request->getBody()->getContents())->token,
+                'data' => json_decode($login_request->getBody()->getContents())], 'عملية ناجحة', 'Success process');
         } else {
             return $this->sendError('خطأ في العملية', 'process failed');
         }
