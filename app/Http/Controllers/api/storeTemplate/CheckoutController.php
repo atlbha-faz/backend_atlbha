8701f1e36b3b5a20fae39913faa6e4ba641c8a8a
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-04-20 11:57:45 +0300

    update

--- app/Http/Controllers/api/storeTemplate/CheckoutController.php
+++ app/Http/Controllers/api/storeTemplate/CheckoutController.php
@@ -636,8 +636,8 @@ class CheckoutController extends BaseController
             ];
 
             $supplier = new FatoorahServices();
-        
-            $response = $supplier->refund('v2/MakeRefund', 'POST', $data);
+            $data = json_encode($data);
+            $response = $supplier->buildRequest('v2/MakeRefund', 'POST', $data);
 
             if ($response['IsSuccess'] == false) {
                 return $this->sendError("خطأ في الارجاع", $supplierCode->ValidationErrors[0]->Error);
