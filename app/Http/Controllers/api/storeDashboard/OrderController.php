507b5af8507c4cd8c6fadcee48f3fc07115015c2
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-04-24 15:39:06 +0300

    update etlobha_show

--- app/Http/Controllers/api/storeDashboard/OrderController.php
+++ app/Http/Controllers/api/storeDashboard/OrderController.php
@@ -156,68 +156,7 @@ class OrderController extends BaseController
                     return $this->sendResponse($success, 'تم تعديل الطلب', 'order update successfully');
                 }
             }
-            if ($request->status === "refund") {
-                if ($order->shippingtype->id == 5) {
-                    $order->update([
-                        'order_status' => $request->status,
-                    ]);
-                    foreach ($order->items as $orderItem) {
-                        $orderItem->update([
-                            'order_status' => $request->status,
-                        ]);
-                    }
-                    $success['orders'] = new OrderResource($order);
-                    $success['status'] = 200;
-                    return $this->sendResponse($success, 'تم تعديل الطلب', 'order update successfully');
-                } else {
-                    $shipping = $shipping_companies[$order->shippingtype->id];
-                    return $shipping->refundOrder($data);
-                    $payment = Payment::where('orderID', $order->id)->first();
-                    if ($payment != null) {
-                        $shipping_price = shippingtype_store::where('shippingtype_id', $order->shippingtype_id)->where('store_id', auth()->user()->store_id)->first();
-                        if ($shipping_price == null) {
-                            $shipping_price = 35;
-                            $extraprice = 2;
-                        } else {
-                            $overprice = $shipping_price->overprice;
-                            $shipping_price = $shipping_price->price;
-                            $extraprice = $overprice;
-                        }if ($order->weight > 15) {
-                            $default_extra_price = ($order->weight - 15) * 2;
-                            $extra_shipping_price = ($order->weight - 15) * $extraprice;
-                        } else {
-                            $extra_shipping_price = 0;
-                            $default_extra_price = 0;
-                        }
-                        $total_price_without_shipping = ($order->total_price) - ($shipping_price) - ($extra_shipping_price);
-                        $data = [
-
-                            "Key" => $payment->paymentTransectionID,
-                            "KeyType" => "invoiceid",
-                            "RefundChargeOnCustomer" => false,
-                            "ServiceChargeOnCustomer" => false,
-                            "Amount" => $total_price_without_shipping,
-                            "Comment" => "refund to the customer",
-                            "AmountDeductedFromSupplier" => 0,
-                            "CurrencyIso" => "SA",
-                        ];
-
-                        $supplier = new FatoorahServices();
-                        $supplierCode = $supplier->buildRequest('v2/MakeRefund', 'POST', $data);
-
-                        if ($supplierCode->IsSuccess == false) {
-                            return $this->sendError("خطأ في الارجاع", $supplierCode->ValidationErrors[0]->Error);
-                        } else {
-                            $success['test'] = $supplierCode;
-                        }
-
-                    }
-                    $success['shipping'] = new shippingResource($shipping);
-                    $success['orders'] = new OrderResource($order);
-                    $success['status'] = 200;
-                    return $this->sendResponse($success, 'تم تعديل الطلب', 'order update successfully');
-                }
-            }
+          
         }
     }
 
