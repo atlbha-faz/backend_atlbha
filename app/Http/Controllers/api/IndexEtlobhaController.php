53e82051c34c8fa0cc865e997d0117a64ec7fdce
ahmed <aattya@goatar.com>
2024-05-15 14:25:32 +0300

    home page products

--- app/Http/Controllers/api/IndexEtlobhaController.php
+++ app/Http/Controllers/api/IndexEtlobhaController.php
@@ -65,7 +65,7 @@ class IndexEtlobhaController extends BaseController
             $success['products'] = AtlbhaIndexProductResource::collection(Product::with(['store' => function ($query) {
                 $query->select('id', 'domain', 'store_name');
             }, 'category' => function ($query) {
-                $query->select('id', 'name');},'importproduct'])->where('is_deleted', 0)->where('admin_special', 'special')->select('id', 'name', 'status', 'cover', 'special', 'admin_special', 'store_id', 'created_at', 'category_id', 'subcategory_id', 'selling_price', 'stock')->get());
+                $query->select('id', 'name');},'importproduct'])->where('is_deleted', 0)->where('admin_special', 'special')->select('id', 'name', 'status', 'cover', 'special', 'admin_special', 'store_id', 'created_at', 'category_id', 'subcategory_id', 'selling_price', 'stock','short_description')->get());
         } else {
             $success['products'] = array();
         }
