ac91a1151eb1d9d641e6642c384883a53dd19f6a
ahmed <aattya@goatar.com>
2024-05-19 17:56:41 +0300

    fix code indexing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
     {
 
         $this->call([
-         ActivitySeeder::class,
+         ReturnReasonsSeeder::class,
             CountrySeeder::class,
             RegionSeeder::class,
             CitySeeder::class,
