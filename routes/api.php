ae5ee4f390c4d39a7ecfb2f2729dc274815ce414
ahmed <aattya@goatar.com>
2024-05-19 14:50:01 +0300

    store user token

--- routes/api.php
+++ routes/api.php
@@ -155,6 +155,7 @@ Route::group([
 // ,AdminCheckPermission::class
 // change status routers
 Route::get('store_token/{id}',[\App\Http\Controllers\api\adminDashboard\StoreController::class,'storeToken']);
+Route::get('store_token',[\App\Http\Controllers\api\adminDashboard\StoreController::class,'getStoreToken']);
 Route::middleware([AdminUser::class])->group(function () {
     Route::prefix('/Admin')->group(function () {
 
