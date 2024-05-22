db2e9d035454aa5cd19cda1a4c157f0469765261
ahmed <aattya@goatar.com>
2024-05-22 12:14:31 +0300

    category and category products

--- app/Http/Middleware/OwnCors.php
+++ app/Http/Middleware/OwnCors.php
@@ -16,8 +16,7 @@ class OwnCors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
+        return $next($request)->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', '*')
             ->header('Access-Control-Allow-Credentials', true)
             ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
