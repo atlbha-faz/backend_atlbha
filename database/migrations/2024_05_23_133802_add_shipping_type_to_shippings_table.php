498488a898cc575c95e8635e08588e54de233d9b
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-05-23 16:39:38 +0300

    add shipping_type

---
+++ database/migrations/2024_05_23_133802_add_shipping_type_to_shippings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shippings', function (Blueprint $table) {
+            $table->enum('shipping_type', [ 'send', 'return'])->default('send');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('shippings', function (Blueprint $table) {
+            $table->dropColumn('shipping_type');
+
+        });
+    }
+};
