51923a64a2bac96bc8faf58deef594a2b06076de
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-06-11 10:38:54 +0300

    update

--- app/Http/Controllers/api/storeDashboard/CheckoutController.php
+++ app/Http/Controllers/api/storeDashboard/CheckoutController.php
@@ -203,9 +203,9 @@ class CheckoutController extends BaseController
         if ($order->paymentype_id == 1 && $order->shippingtype_id == 1) {
             $customer = User::where('id', $order->user_id)->where('is_deleted', 0)->first();
             $paymenttype = Paymenttype::where('id', $order->paymentype_id)->first();
-            $total_price_without_shipping = ($order->total_price) - ($order->shipping_price) - ($order->overweight_price);
-            $deduction = ($total_price_without_shipping * 0.01) + 1;
-            $price_after_deduction = $total_price_without_shipping - $deduction;
+            $vat=$order->total_price*0.15;
+            $deduction = ($order->total_price * 0.01) + 1 +$vat;
+            $price_after_deduction = $order->total_price - ($order->shipping_price) - ($order->overweight_price) - $deduction;
 
             $processingDetails = [
                 "AutoCapture" => true,
