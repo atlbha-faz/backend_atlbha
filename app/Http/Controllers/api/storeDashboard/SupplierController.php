021c3cb220c4d6e189ef3691fd299be37ae7f02f
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-06-12 14:27:48 +0300

    update supplier

--- app/Http/Controllers/api/storeDashboard/SupplierController.php
+++ app/Http/Controllers/api/storeDashboard/SupplierController.php
@@ -195,7 +195,8 @@ class SupplierController extends BaseController
             'bankAccount' => $request->input('bankAccount'),
             'iban' => $request->input('iban'),
             'supplierCode' => $supplierCode['Data']['SupplierCode'],
-            'status' => "Pending"
+            'status' => "Pending",
+            'comment'=>''
         ]);
         $storeAdmain->update(['supplierCode' => $supplierCode['Data']['SupplierCode']]);
 
