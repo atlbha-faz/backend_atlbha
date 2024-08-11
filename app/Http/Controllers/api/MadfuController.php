fbccb9f5a37abc143b78d65a4d79e3349223bbf2
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-08-11 09:21:24 +0300

    update

--- app/Http/Controllers/api/MadfuController.php
+++ app/Http/Controllers/api/MadfuController.php
@@ -175,6 +175,12 @@ class MadfuController extends BaseController
         $package_store->update([
             'start_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'end_at' => $end_at]);
+        $subscriptions = Package_store::where('store_id', $store->id)->whereNot('payment_status','paid')->get();
+        if ($subscriptions) {
+            foreach ($subscriptions as $subscription) {
+                $subscription->delete();
+            }
+        }
 
     }
 
