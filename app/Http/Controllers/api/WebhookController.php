b81acd5621fdbcf8cbeaec149a1feef7970f172e
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-08-22 14:41:05 +0300

    update

--- app/Http/Controllers/api/WebhookController.php
+++ app/Http/Controllers/api/WebhookController.php
@@ -220,14 +220,6 @@ class WebhookController extends BaseController
         $package_store->update([
             'start_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'end_at' => $end_at]);
-        $subscriptions = Package_store::where('store_id', $store->id)->where('payment_status',null)->get();
-        if ($subscriptions) {
-            if ($subscriptions) {
-                foreach ($subscriptions as $subscription) {
-                    $subscription->delete();
-                }
-            }
-        }
 
     }
 
