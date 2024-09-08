f33091c2ffa3b43c84d2fec98b4e90bfff94864b
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-09-08 11:27:01 +0300

    update

--- app/Rules/ValidTimestamp.php
+++ app/Rules/ValidTimestamp.php
@@ -26,17 +26,7 @@ class ValidTimestamp implements Rule
     public function passes($attribute, $value)
     {
          // Regular expression to match the expected format
-         $pattern = '/^(\d{13})([+-]\d{4})$/';
-
-         if (preg_match($pattern, $value, $matches)) {
-             $milliseconds = (int)$matches[1]; // Extract milliseconds
-             $timezoneOffset = $matches[2]; // Extract timezone offset
- 
-             // Validate milliseconds (should be a positive integer)
-             return $milliseconds >= 0 && preg_match('/^[+-]\d{4}$/', $timezoneOffset);
-         }
- 
-         return false; // Does not match the expected format
+         return preg_match('/^(\d{13})([+-]\d{4})$/', $value);
     }
 
     /**
