8cb5c9bb31de2184f6baf6c5818c732bc080f4b3
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-09-10 12:38:04 +0300

    update

---
+++ app/Models/Trip.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Trip extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'description', 'image', 'row_order' , 'package_id'];
+    public function package()
+    {
+        return $this->belongsTo(Package::class, 'package_id');
+    }
+    public function setImageAttribute($image)
+    {
+        if (!is_null($image)) {
+            if (gettype($image) != 'string') {
+                $i = $image->store('images/trip', 'public');
+                $this->attributes['image'] = $image->hashName();
+            } else {
+                $this->attributes['image'] = $image;
+            }
+        }
+    }
+
+    public function getImageAttribute($image)
+    {
+        if (is_null($image)) {
+            return   asset('assets/media/man.png');
+        }
+        return asset('storage/images/trip') . '/' . $image;
+    }
+}
