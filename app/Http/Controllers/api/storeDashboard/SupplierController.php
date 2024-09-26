004e65f958cb5df51fce0cddfe4882fe159bffd5
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-09-26 13:32:43 +0300

    update

--- app/Http/Controllers/api/storeDashboard/SupplierController.php
+++ app/Http/Controllers/api/storeDashboard/SupplierController.php
@@ -66,7 +66,10 @@ class SupplierController extends BaseController
     }
     public function store(Request $request)
     {
-       
+        $store = Store::where('id', auth()->user()->store_id)->whereNot('verification_status', 'accept')->first();
+        if ($store) {
+            return $this->sendError("المتجر  غير موثق ", "store is't verified");
+        }
         $storeAdmain = User::where('user_type', 'store')->where('is_deleted', 0)->where('store_id', auth()->user()->store_id)->first();
         $store = Store::where('is_deleted', 0)->where('id', auth()->user()->store_id)->first();
         $account = Account::where('store_id', auth()->user()->store_id)->first();
@@ -163,6 +166,10 @@ class SupplierController extends BaseController
     }
     public function update(Request $request)
     {
+        $store = Store::where('id', auth()->user()->store_id)->whereNot('verification_status', 'accept')->first();
+        if ($store) {
+            return $this->sendError("المتجر  غير موثق ", "store is't verified");
+        }
         $storeAdmain = User::where('user_type', 'store')->where('is_deleted', 0)->where('store_id', auth()->user()->store_id)->first();
         $store = Store::where('is_deleted', 0)->where('id', auth()->user()->store_id)->first();
         $account = Account::where('store_id', auth()->user()->store_id)->first();
