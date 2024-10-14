19c3ea648ae115acce87d614226f3f905f8c8a9b
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-10-14 11:18:10 +0300

    update

--- app/Http/Controllers/api/adminDashboard/AdminOrderController.php
+++ app/Http/Controllers/api/adminDashboard/AdminOrderController.php
@@ -2,20 +2,14 @@
 
 namespace App\Http\Controllers\api\adminDashboard;
 
-use App\Models\User;
-use App\Models\Order;
-use App\Models\Store;
-use App\Models\Option;
 use App\Helpers\Helper;
-use App\Models\Product;
-use App\Models\Shipping;
-use Illuminate\Http\Request;
-use App\Models\Importproduct;
-use App\Http\Resources\OrderResource;
-use App\Http\Resources\shippingResource;
-use Illuminate\Support\Facades\Validator;
-use App\Services\ShippingComanies\AramexCompanyService;
 use App\Http\Controllers\api\BaseController as BaseController;
+use App\Http\Resources\OrderResource;
+use App\Models\Order;
+use App\Rules\ValidTimestamp;
+use App\Services\ShippingComanies\AramexCompanyService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AdminOrderController extends BaseController
 {
@@ -25,31 +19,31 @@ class AdminOrderController extends BaseController
     }
     public function index(Request $request)
     {
-        $success['new'] = Order::where('store_id', null)->where('is_deleted', 0)->where('is_archive',0)->where(function ($sub_query) {
-            $sub_query->where('paymentype_id',null)->orWhere('payment_status','paid');
-             
+        $success['new'] = Order::where('store_id', null)->where('is_deleted', 0)->where('is_archive', 0)->where(function ($sub_query) {
+            $sub_query->where('paymentype_id', null)->orWhere('payment_status', 'paid');
+
         })->where('order_status', 'new')->count();
-        $success['completed'] = Order::where('store_id', null)->where('is_deleted', 0)->where('is_archive',0)->where(function ($sub_query) {
-            $sub_query->where('paymentype_id',null)->orWhere('payment_status','paid');
-             
+        $success['completed'] = Order::where('store_id', null)->where('is_deleted', 0)->where('is_archive', 0)->where(function ($sub_query) {
+            $sub_query->where('paymentype_id', null)->orWhere('payment_status', 'paid');
+
         })->where('order_status', 'completed')->count();
 
-        $success['not_completed'] = Order::where('store_id', null)->where('is_deleted', 0)->where('is_archive',0)->where(function ($sub_query) {
-            $sub_query->where('paymentype_id',null)->orWhere('payment_status','paid');
-             
+        $success['not_completed'] = Order::where('store_id', null)->where('is_deleted', 0)->where('is_archive', 0)->where(function ($sub_query) {
+            $sub_query->where('paymentype_id', null)->orWhere('payment_status', 'paid');
+
         })->where('order_status', 'not_completed')->count();
         $success['canceled'] = Order::whereHas('items', function ($q) {
             $q->where('store_id', null)->where('order_status', 'canceled');
-        })->where('is_archive',0)->where(function ($sub_query) {
-            $sub_query->where('paymentype_id',null)->orWhere('payment_status','paid');
-             
+        })->where('is_archive', 0)->where(function ($sub_query) {
+            $sub_query->where('paymentype_id', null)->orWhere('payment_status', 'paid');
+
         })->count();
 
         $success['all'] = Order::whereHas('items', function ($q) {
             $q->where('store_id', null);
-        })->where('is_deleted', 0)->where('is_archive',0)->where(function ($sub_query) {
-            $sub_query->where('paymentype_id',null)->orWhere('payment_status','paid');
-             
+        })->where('is_deleted', 0)->where('is_archive', 0)->where(function ($sub_query) {
+            $sub_query->where('paymentype_id', null)->orWhere('payment_status', 'paid');
+
         })->count();
         $count = ($request->has('number') && $request->input('number') !== null) ? $request->input('number') : 10;
 
@@ -57,11 +51,11 @@ class AdminOrderController extends BaseController
             $query->select('id', 'city_id');
         }, 'shippings', 'items' => function ($query) {
             $query->select('id');
-        }])->where('store_id', null)->where('is_deleted', 0)->where('is_archive',0)->where(function ($sub_query) {
-            $sub_query->where('paymentype_id',null)->orWhere('payment_status','paid');
-             
+        }])->where('store_id', null)->where('is_deleted', 0)->where('is_archive', 0)->where(function ($sub_query) {
+            $sub_query->where('paymentype_id', null)->orWhere('payment_status', 'paid');
+
         })->orderByDesc('id')->select(['id', 'user_id', 'order_number', 'total_price', 'quantity', 'created_at', 'order_status']);
-        $data= $data->paginate($count);
+        $data = $data->paginate($count);
         $success['orders'] = OrderResource::collection($data);
         $success['page_count'] = $data->lastPage();
         $success['current_page'] = $data->currentPage();
@@ -101,63 +95,72 @@ class AdminOrderController extends BaseController
 
             'city' => 'required_if:status,==,ready',
             'street_address' => 'required_if:status,==,ready',
+            'pickup_date' => ['required_if:status,==,delivery_in_progress', 'required_if:status,ready', new ValidTimestamp],
         ]);
         if ($validator->fails()) {
             # code...
             return $this->sendError(null, $validator->errors());
         }
 
-            if ($request->status === "completed") {
-                Helper::orderProductShow($order->id);
-            }
-            $shipping_companies = [
-                1 => new AramexCompanyService(),
+        if ($request->status === "completed") {
+            Helper::orderProductShow($order->id);
+        }
+        $shipping_companies = [
+            1 => new AramexCompanyService(),
+        ];
+        if ($request->status == "canceled") {
+
+            $shipping = $shipping_companies[$order->shippingtype->id];
+            $success['orders'] = $shipping->cancelOrder($order->id);
+            $success['status'] = 200;
+            return $this->sendResponse($success, 'تم إلغاء الطلب', 'order canceled successfully');
+
+        } else {
+            $data = [
+                "shipper_line1" => $request->street_address,
+                "shipper_line2" => $request->street_address,
+                "shipper_city" => $request->city,
+                "shipper_district" => $request->district,
+                "shipper_name" => "atlbha",
+                "shipper_comany" => "atlbha",
+                "shipper_name" => "atlbha",
+                "shipper_phonenumber" => "00966506340450",
+                "shipper_email" => "help@atlbha.sa",
+                "order_id" => $order->id,
+                "pickup_date" => $request->pickup_date,
+
             ];
-            if ($request->status == "canceled") {
+            if ($request->status === "ready") {
 
                 $shipping = $shipping_companies[$order->shippingtype->id];
-                $success['orders'] = $shipping->cancelOrder($order->id);
-                $success['status'] = 200;
-                return $this->sendResponse($success, 'تم إلغاء الطلب', 'order canceled successfully');
-    
+                $response = $shipping->createOrder($data);
+
+                return $response;
+            }
+            if ($request->status === "delivery_in_progress") {
+
+                $shipping = $shipping_companies[$order->shippingtype->id];
+
+                $response = $shipping->createPickup($data);
+
+                return $response;
             } else {
-                $data = [
-                    "shipper_line1" => $request->street_address,
-                    "shipper_line2" => $request->street_address,
-                    "shipper_city" => $request->city,
-                    "shipper_district" => $request->district,
-                    "shipper_name" => "atlbha",
-                    "shipper_comany" => "atlbha",
-                    "shipper_name" => "atlbha",
-                    "shipper_phonenumber" =>"00966506340450",
-                    "shipper_email" => "help@atlbha.sa",
-                    "order_id" => $order->id,
-    
-                ];
-                if ($request->status === "ready") {
-    
-                    $shipping = $shipping_companies[$order->shippingtype->id];
-                    $success['orders'] = $shipping->createOrder($data);
-                    $success['status'] = 200;
-                    return $this->sendResponse($success, 'تم تعديل الطلب', 'order update successfully');
-    
-                } else {
-                    $order->update([
+                $order->update([
+                    'order_status' => $request->status,
+                ]);
+                foreach ($order->items as $orderItem) {
+                    $orderItem->update([
                         'order_status' => $request->status,
                     ]);
-                    foreach ($order->items as $orderItem) {
-                        $orderItem->update([
-                            'order_status' => $request->status,
-                        ]);
-                    }
-    
-                    $success['orders'] = new OrderResource($order);
-                    $success['status'] = 200;
-                    return $this->sendResponse($success, 'تم تعديل الطلب', 'order update successfully');
-    
                 }
-            }    
-        
+
+                $success['orders'] = new OrderResource($order);
+                $success['status'] = 200;
+                return $this->sendResponse($success, 'تم تعديل الطلب', 'order update successfully');
+
+            }
+        }
+
     }
     public function deleteAll(Request $request)
     {
@@ -183,14 +186,14 @@ class AdminOrderController extends BaseController
         $query = $request->input('query');
         $count = ($request->has('number') && $request->input('number') !== null) ? $request->input('number') : 10;
 
-        $orders = Order::where('is_deleted', 0)->where('store_id', null)->where(function ($q) use($query) {
+        $orders = Order::where('is_deleted', 0)->where('store_id', null)->where(function ($q) use ($query) {
             $q->whereHas('user', function ($userQuery) use ($query) {
-            $userQuery->whereHas('store', function ($storeQuery) use ($query) {
-                $storeQuery->where('store_name', 'like', "%$query%");
-            });
-        })->orWhere('order_number', 'like', "%$query%");
-    })
-            ->where('is_archive',0)->where('payment_status','paid')->orderBy('created_at', 'desc')
+                $userQuery->whereHas('store', function ($storeQuery) use ($query) {
+                    $storeQuery->where('store_name', 'like', "%$query%");
+                });
+            })->orWhere('order_number', 'like', "%$query%");
+        })
+            ->where('is_archive', 0)->where('payment_status', 'paid')->orderBy('created_at', 'desc')
             ->paginate($count);
 
         $success['query'] = $query;
