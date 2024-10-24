3d6ff7b1b82d87d4626ad00514ae62c6b10218f8
RawaaSaif-FAZ <rawaa.faz.it@gmail.com>
2024-10-24 15:52:17 +0300

    update link

--- app/Http/Resources/ProductAdaminResource.php
+++ app/Http/Resources/ProductAdaminResource.php
@@ -52,11 +52,12 @@ class ProductAdaminResource extends JsonResource
                 'status' => $status,
                 'special' => $special,
                 'admin_special' => $admin_special,
-                'url' => 'https://' . $domain . '/shop/product/' . $this->id.'/'.preg_replace('/ /', '-',$this->name),
+                'url' =>($this->is_service==1)? 'https://' . $domain . '/services/'.$this->id.'/' .preg_replace('/ /', '-',$this->name) : 'https://' . $domain . '/products/'.preg_replace('/ /', '-',$this->name).'/'. $this->id,
                 'created_at' => (string) $this->created_at,
                 'updated_at' => (string) $this->updated_at,
                 'category' => new CategoryResource($this->category),
                 'store' => new StoreResource($this->store),
+                'is_service'=> ($this->is_service==1) ? true : false,
                 'type' => $type,
                 'is_import' => $import,
 
